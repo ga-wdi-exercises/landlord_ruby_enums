e8cc2d3a1e36225732e674427e23afed0ab7a80d
Matt <mvaden08@gmail.com>
2016-04-07 16:54:18 -0400

    Lots of tenants dude

--- app.rb
+++ app.rb
@@ -4,16 +4,41 @@ tenants = data[:tenants]
 
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
+
   # What are the properties for each of the two types of hashes
 
+
 # Use enumerables to -
+
   # Print all the addresses for the apartments
+homes = each.do |address|
+  puts address [:apartments]
+end
+
   # Print all the names for tenants
+tenants = each.do |name|
+  puts tenants[:name]
+end
+
   # Print only apartments that are less then 700 in rent
+rent = each.sort_by |monthly_rent|
+ puts rent[:monthly_rent]
+end
+
   # Print only tenants that are over the age of 44
+
+
   # Print only tenants that have an apartment id of 1
+
+
   # Print all the tenants in order from youngest to oldest
+  tenants.
+
+
   # Print the names of all the tenants alphabetically
+  tenants.sort_by do |name|
+    puts name[:]
+  end
 
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
