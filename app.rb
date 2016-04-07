24dcfbdf2c34bc17be82a1818ee08b91f1c99536
Leland Jordon <lelandjordon@mac.com>
2016-04-07 17:26:19 -0400

    Can print addresses, names, and cheap apartments.

--- app.rb
+++ app.rb
@@ -2,14 +2,33 @@ require_relative "data"
 apartments = data[:apartments]
 tenants = data[:tenants]
 
+
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
   # What are the properties for each of the two types of hashes
 
 # Use enumerables to -
   # Print all the addresses for the apartments
+  all_addresses = apartments.map do |n|
+     n[:address]
+  end
+  puts all_addresses
+
   # Print all the names for tenants
-  # Print only apartments that are less then 700 in rent
+  all_names = tenants.map do |n|
+    n[:name]
+  end
+  puts all_names.inspect
+
+  # # Print only apartments that are less then 700 in rent
+  # cheap_apartments = apartments.each do |n|
+  #   apartments.select {n[:monthly_rent] < 700}
+  # end
+  #  This one ^^^^^ doesn't work and I can't figure out why.
+
+ cheap_apartments = apartments.select {|a| a[:monthly_rent] < 700 }
+  puts cheap_apartments
+
   # Print only tenants that are over the age of 44
   # Print only tenants that have an apartment id of 1
   # Print all the tenants in order from youngest to oldest
