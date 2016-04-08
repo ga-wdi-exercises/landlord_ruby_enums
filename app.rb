38d09dc538d5536550565f1fcb64db962976fc35
Joshua <Joshua.Nezianya@gmail.com>
2016-04-08 07:18:28 -0400

    mvp

--- app.rb
+++ app.rb
@@ -1,20 +1,50 @@
 require_relative "data"
 apartments = data[:apartments]
 tenants = data[:tenants]
+#
+# # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
+#   # Explain how the data is structured
+#   # What are the properties for each of the two types of hashes
+#
+# # Use enumerables to -
+#   # Print all the addresses for the apartments
+# # puts apartments
+#   # Print all the names for tenants
+# # puts tenants
+#   # Print only apartments that are less then 700 in rent
+#
+# cheap_apartments = apartments.find_all {|a| a[:monthly_rent]< 700}
+#
+#   puts cheap_apartments
+#   # Print only tenants that are over the age of 44
+# older_tenants = tenants.find_all{|t| t[:age] > 40}
+#   puts older_tenants
+#   # Print only tenants that have an apartment id of 1
+# tenant_ids = tenants.select { |a| a[:apartment_id] == 1}
+#   puts tenant_ids
+#   # Print all the tenants in order from youngest to oldest
+# # order_tenants = tenants.sort_by{|b| b[:name]}
+#   # puts order_tenants
+#   # Print the names of all the tenants alphabetically
+#   # abc_tenants = tenants.sort_by{|b| b[:name].downcase}
+#     # puts abc_tenants
+#   ## More challenging
+#   # When printing tenants also print out the address that the tenant resides in.
+#   # When printing all apartments, under each apartment print all of its tenants
+print "1.) apartments overview"
+print "2.) tenants overview"
+print "please enter your choice:"
 
-# First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
-  # Explain how the data is structured
-  # What are the properties for each of the two types of hashes
+input = gets.chomp
 
-# Use enumerables to -
-  # Print all the addresses for the apartments
-  # Print all the names for tenants
-  # Print only apartments that are less then 700 in rent
-  # Print only tenants that are over the age of 44
-  # Print only tenants that have an apartment id of 1
-  # Print all the tenants in order from youngest to oldest
-  # Print the names of all the tenants alphabetically
+if input == "1"
+  apartments.map do |unit|
+    puts unit
+  end
+end
 
-  ## More challenging
-  # When printing tenants also print out the address that the tenant resides in.
-  # When printing all apartments, under each apartment print all of its tenants
+if input == "2"
+  tenants.map do |person|
+    puts person
+  end
+end
