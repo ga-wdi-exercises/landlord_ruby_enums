b48fafda7d2ecef50a28c3900ccd1abc760c2e4c
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-04-07 22:07:13 -0400

    adds solution for Part 1

--- app.rb
+++ app.rb
@@ -5,15 +5,44 @@ tenants = data[:tenants]
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
   # What are the properties for each of the two types of hashes
+    # The data is held inside a function that returns a hash with two key value (where the value is an array of objects) pairs: "apartments" and "tenants". Each hash in the apartments array contains the keys id, address, monthly_rent, square_feet. Each hash in the tenants array contains the keys id, name, age, and apartment_id.
 
 # Use enumerables to -
   # Print all the addresses for the apartments
+
+all_addresses = apartments.map do |apartment|
+  apartment[:address]
+end
+
+puts all_addresses
+
   # Print all the names for tenants
+
+all_names = tenants.map do |tenant|
+    tenant[:name]
+  end
+
+  puts all_names
+
   # Print only apartments that are less then 700 in rent
+
+puts apartments.select {|apartment| apartment[:monthly_rent] < 700}
+
   # Print only tenants that are over the age of 44
+
+puts tenants.select {|tenant| tenant[:age] > 44}
+
   # Print only tenants that have an apartment id of 1
+
+puts tenants.select {|tenant| tenant[:apartment_id] == 1}
+
   # Print all the tenants in order from youngest to oldest
+
+puts tenants.sort_by {|tenant| tenant[:age]}
+
   # Print the names of all the tenants alphabetically
+
+puts tenants.sort_by {|tenant| tenant[:name].downcase}
 
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
