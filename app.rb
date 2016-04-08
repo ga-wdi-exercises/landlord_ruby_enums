4f730cf19da3e21a51c131d6c44a20bc68d34687
Zaneh Williams <zanehmw@gmail.com>
2016-04-07 20:24:13 -0400

    i think i made the mvp

--- app.rb
+++ app.rb
@@ -2,63 +2,97 @@ require_relative "data"
 apartments = data[:apartments]
 tenants = data[:tenants]
 
+# - I should have a numbered interface for my application so that I can just type in a number to access different parts of my program.
+# - I should be allowed an option to view all the apartments so that I can get an overview of my properties.
+# - I should be allowed  an option view all the tenants so that I can get an overview of my tenants
+
+puts "Hello welcome to this app."
+puts "Type '1' to view all of your apartments"
+puts "Type '2' to view all of your tenants"
+track_number = gets.chomp
+if track_number == "1"
+  puts apartments
+elsif track_number == "2"
+  puts tenants
+else
+  puts "Hello welcome to this app."
+  puts "Type '1' to view all of your apartments"
+  puts "Type '2' to view all of your tenants"
+end
+# puts apartments
+
+
+
+
+
+
+
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
-  Inside of the function named "data" there are two arrays named "apartments" and "tenants". Body of these arrays are comprised of multiple hashes that contain properties about the apartment or tenant
+  # Inside of the function named "data" there are two arrays named "apartments" and "tenants". Body of these arrays are comprised of multiple hashes that contain properties about the apartment or tenant
   # What are the properties for each of the two types of hashes
-  Properties of hashes in apartments array: id, address, monthly_rent, square_feet
-  Properties of hashes in tenants array:  id, name, age, apartment_id
+  # Properties of hashes in apartments array: id, address, monthly_rent, square_feet
+  # Properties of hashes in tenants array:  id, name, age, apartment_id
 
 
 # Use enumerables to -
   # Print all the addresses for the apartments
 
-  apartments.each do |apartment|
-    puts apartment[:address]
-  end
+  # apartments.each do |apartment|
+  #   puts apartment[:address]
+  # end
 
   # Print all the names for tenants
 
-  tenants.each do |namez|
-    puts namez[:name]
-  end
+  # tenants.each do |namez|
+  #   puts namez[:name]
+  # end
 
   # Print only apartments that are less then 700 in rent
 
- cheap_apartments = apartments.select do |apartment|
-   apartment[:monthly_rent] < 700
- end
- puts cheap_apartments
+ # cheap_apartments = apartments.select do |apartment|
+ #   apartment[:monthly_rent] < 700
+ # end
+ # puts cheap_apartments
 
   # Print only tenants that are over the age of 44
 
-old_people = tenants.select do |person|
-  person[:age] > 44
-end
-puts old_people
+# old_people = tenants.select do |person|
+#   person[:age] > 44
+# end
+# puts old_people
 
   # Print only tenants that have an apartment id of 1
 
-  oneid = tenants.select do |person|
-    person[:apartment_id] == 1
-  end
-  puts oneid
+  # oneid = tenants.select do |person|
+  #   person[:apartment_id] == 1
+  # end
+  # puts oneid
 
   # Print all the tenants in order from youngest to oldest
 
-sortage = tenants.sort_by do |tenant|
-  tenant[:age]
-end
-puts sortage
+# sortage = tenants.sort_by do |tenant|
+#   tenant[:age]
+# end
+# puts sortage
 
 
   # Print the names of all the tenants alphabetically
 
-  fromatoz = tenants.sort_by do |tenant|
-    tenant[:name].downcase
-  end
-  puts fromatoz
+  # fromatoz = tenants.sort_by do |tenant|
+  #   tenant[:name].downcase
+  # end
+  # puts fromatoz
 
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
+
+#   newlist = []
+# tenants_with_address = tenants.each do |person|
+#   apartments.each do |apartment|
+#     newlist << [tenants, apartments[:apartment_id]]
+#   end
+# end
+
+
   # When printing all apartments, under each apartment print all of its tenants
