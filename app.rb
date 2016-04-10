7fcf0c34129c4110ea3eca771d38aa0d32bd9654
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-04-10 19:48:38 -0400

    updates ui; adds silver level features

--- app.rb
+++ app.rb
@@ -1,73 +1,57 @@
 require_relative "data"
+require_relative "models/apartment"
+require_relative "models/tenant"
 apartments = data[:apartments]
 tenants = data[:tenants]
 
-# First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
-  # Explain how the data is structured
-  # What are the properties for each of the two types of hashes
-    # The data is held inside a function that returns a hash with two key value (where the value is an array of objects) pairs: "apartments" and "tenants". Each hash in the apartments array contains the keys id, address, monthly_rent, square_feet. Each hash in the tenants array contains the keys id, name, age, and apartment_id.
-#
-# # Use enumerables to -
-#   # Print all the addresses for the apartments
-#
-# all_addresses = apartments.map do |apartment|
-#   apartment[:address]
-# end
-#
-# puts all_addresses
-#
-#   # Print all the names for tenants
-#
-# all_names = tenants.map do |tenant|
-#     tenant[:name]
-#   end
-#
-#   puts all_names
-#
-#   # Print only apartments that are less then 700 in rent
-#
-# puts apartments.select {|apartment| apartment[:monthly_rent] < 700}
-#
-#   # Print only tenants that are over the age of 44
-#
-# puts tenants.select {|tenant| tenant[:age] > 44}
-#
-#   # Print only tenants that have an apartment id of 1
-#
-# puts tenants.select {|tenant| tenant[:apartment_id] == 1}
-#
-#   # Print all the tenants in order from youngest to oldest
-#
-# puts tenants.sort_by {|tenant| tenant[:age]}
-#
-#   # Print the names of all the tenants alphabetically
-#
-# puts tenants.sort_by {|tenant| tenant[:name].downcase}
-#
-#   ## More challenging
-#   # When printing tenants also print out the address that the tenant resides in.
-#   # When printing all apartments, under each apartment print all of its tenants
 userInput = nil
 while userInput != "exit" do
-  puts "[Options Menu] \nType the number or phrase that cooresponds with your choice."
+  puts "[Options Menu] \nType the number or phrase that co4
+  responds with your choice."
   puts "1: View all apartments"
   puts "2: View all tenants"
+  puts "3: View all apartments with occupancy"
+  puts "4: View all tenants with address"
   puts "Exit: Close program"
   userInput = gets.chomp.downcase
 
-        if userInput == "1"
-            all_apartments = apartments.map do |apartment|
-              apartment
-            end
+      if userInput == "1"
+        apartments.map do |apartment|
+          puts "Address: " + apartment[:address]
+          puts "Size: " + "#{apartment[:square_feet]}" + " sq ft"
+          puts "Monthly Rent: $" + "#{apartment[:monthly_rent]}"
+          puts "=" * 50
+        end
 
-            puts all_apartments
+      elsif userInput == "2"
+        tenants.map do |tenant|
+            puts "Name: " + tenant[:name]
+            puts "Age: " + "#{tenant[:age]}"
+            puts "=" * 50
+        end
 
-        elsif userInput == "2"
-          all_tenants = tenants.map do |tenant|
-              tenant
-            end
+      elsif userInput == "3"
+        apartments.each do |apartment|
+          puts "=" * 50
+          puts apartment[:address]
+          puts "=" * 50
+          tenants_of_apartment = tenants.select{|tenant| tenant[:apartment_id] == apartment[:id]}
+          tenants_of_apartment.each do |tenant|
+            puts tenant[:name]
+          end
+          puts nil
+        end
 
-            puts all_tenants
+      elsif userInput == "4"
+        tenants.each do |tenant|
+          tenants_apartment_id = tenant[:apartment_id]
+          matching_apartments = apartments.find do |apartment|
+            apartment[:id] == tenants_apartment_id
+        end
+
+        puts "#{tenant[:name]} | #{matching_apartments[:address]}".center(50)
+        puts "=" * 50
+        end
 
   end
 end
