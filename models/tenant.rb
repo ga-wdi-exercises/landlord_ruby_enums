3e985afa207da712301ec1e9f970a0b472de431f
Wes Reed <wesley.a.reed@gmail.com>
2016-04-11 10:06:47 -0400

    Refactor to att accessor

--- models/tenant.rb
+++ models/tenant.rb
@@ -1,25 +1,10 @@
 class Tenant
+  attr_accessor :id, :name, :age, :apartment_id
+
   def initialize (id, name, age, apartment_id)
     @id = id
     @name = name
     @age = age
     @apartment_id = apartment_id
-  end
-
-  def id
-    return @id
-  end
-
-  def name
-    return @name
-  end
-
-  def age
-    return @age
-  end
-
-  def apartment_id
-    return @apartment_id
-  end
-  
+  end  
 end
