aaa1175055e22330039866f74ff44987c473eca4
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-04-11 13:38:18 -0400

    converts content of arrays from hashes to objects, updates code for new arrays

--- app.rb
+++ app.rb
@@ -3,6 +3,18 @@ require_relative "models/apartment"
 require_relative "models/tenant"
 apartments = data[:apartments]
 tenants = data[:tenants]
+ruby_apartments = []
+ruby_tenants = []
+
+
+ruby_apartments = apartments.map do |apartment|
+  Apartment.new(apartment[:id], apartment[:address], apartment[:monthly_rent], apartment[:square_feet])
+end
+
+ruby_tenants = tenants.map do |tenant|
+  Tenant.new(tenant[:id], tenant[:name], tenant[:age], tenant[:apartment_id])
+end
+
 
 userInput = nil
 while userInput != "exit" do
@@ -16,40 +28,40 @@ while userInput != "exit" do
   userInput = gets.chomp.downcase
 
       if userInput == "1"
-        apartments.map do |apartment|
-          puts "Address: " + apartment[:address]
-          puts "Size: " + "#{apartment[:square_feet]}" + " sq ft"
-          puts "Monthly Rent: $" + "#{apartment[:monthly_rent]}"
+        ruby_apartments.map do |apartment|
+          puts "Address: " + apartment.address
+          puts "Size: " + "#{apartment.square_feet}" + " sq ft"
+          puts "Monthly Rent: $" + "#{apartment.monthly_rent}"
           puts "=" * 50
         end
 
       elsif userInput == "2"
-        tenants.map do |tenant|
-            puts "Name: " + tenant[:name]
-            puts "Age: " + "#{tenant[:age]}"
+        ruby_tenants.map do |tenant|
+            puts "Name: " + tenant.name
+            puts "Age: " + "#{tenant.age}"
             puts "=" * 50
         end
 
       elsif userInput == "3"
-        apartments.each do |apartment|
+        ruby_apartments.each do |apartment|
           puts "=" * 50
-          puts apartment[:address]
+          puts apartment.address
           puts "=" * 50
-          tenants_of_apartment = tenants.select{|tenant| tenant[:apartment_id] == apartment[:id]}
+          tenants_of_apartment = ruby_tenants.select{|tenant| tenant.apartment_id == apartment.id}
           tenants_of_apartment.each do |tenant|
-            puts tenant[:name]
+            puts tenant.name
           end
           puts nil
         end
 
       elsif userInput == "4"
-        tenants.each do |tenant|
-          tenants_apartment_id = tenant[:apartment_id]
-          matching_apartments = apartments.find do |apartment|
-            apartment[:id] == tenants_apartment_id
+        ruby_tenants.each do |tenant|
+          tenants_apartment_id = tenant.apartment_id
+          matching_apartments = ruby_apartments.find do |apartment|
+            apartment.id == tenants_apartment_id
         end
 
-        puts "#{tenant[:name]} | #{matching_apartments[:address]}".center(50)
+        puts "#{tenant.name} | #{matching_apartments.address}".center(50)
         puts "=" * 50
         end
 
