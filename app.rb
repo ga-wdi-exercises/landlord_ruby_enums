503bbc41db8573c404fc903bb7ff6441c13b7889
Marc Fournier <marc.fournier91@gmail.com>
2016-07-18 19:50:19 -0400

    Answered the written questions

--- app.rb
+++ app.rb
@@ -4,7 +4,13 @@ tenants = data[:tenants]
 
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
+  #The return value for data is a hash. The first key is associated with the first array. The second key
+  #is associated with the second. Thus the return value essentially makes data(which was two arrays consisting of hashes) into a hash with two keys.
+  #So our data is a hash with two keys. Each key is an array of hashes.
+
   # What are the properties for each of the two types of hashes
+  #ID, address, monthly_rent, square_feet
+  #ID,name,age,apartment_id
 
 # Use enumerables to -
   # Print all the addresses for the apartments
