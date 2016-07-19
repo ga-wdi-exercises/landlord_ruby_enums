4eb4b326e12b4624a6e28efcd12d7361cc5be209
Christopher Phillips <cphillips918@gmail.com>
2016-07-18 23:26:39 -0400

    glad I saved this one for last!

--- app.rb
+++ app.rb
@@ -4,7 +4,13 @@ tenants = data[:tenants]
 
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
-  # What are the properties for each of the two types of hashes
+  # - the data is sorted into two large arrays that consist of hashes
+  # - the first is an apartments array consisting of hashes with 4 key values
+  # - the second is a tenant array that consists of object also with 4 key values
+  # - these two arrays are returned in a large hash holder represented by calling the defined data
+  # What are the properties for each of the two types of hashes?
+  # - Apartments Array: id-address-rent-sqft
+  # - Tenants Array - id-name-age-apartmentid
 
 # Use enumerables to -
   # Print all the addresses for the apartments
@@ -18,3 +24,17 @@ tenants = data[:tenants]
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
   # When printing all apartments, under each apartment print all of its tenants
+apartments.each {|unit| puts unit[:address]}
+tenants.each{|tenant| puts tenant[:name]}
+
+apartments.each do |unit|
+    if unit[:monthly_rent] < 700
+        puts unit
+    end
+end
+
+tenants.each do |tenant|
+    if tenant[:age]>44
+        puts tenant
+    end
+end
