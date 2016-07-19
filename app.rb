ebed4de12bfcbeffef3b2b375091ea856e6e6fde
Chase <chasefreeman13@gmail.com>
2016-07-19 14:41:20 -0400

    finished assignment

--- app.rb
+++ app.rb
@@ -7,13 +7,49 @@ tenants = data[:tenants]
   # What are the properties for each of the two types of hashes
 
 # Use enumerables to -
-  # Print all the addresses for the apartments
+
+# Print all the addresses for the apartments
+
+        # apartments.each do |apartment|
+        #   puts apartment[:address]
+        # end
+
   # Print all the names for tenants
+
+        # tenants.each do |tenant|
+        #   puts tenant[:name]
+        # end
+
   # Print only apartments that are less then 700 in rent
+
+        # apartments.each do |apartment|
+        #     if apartment[:monthly_rent] < 700
+        #       puts apartment[:address]
+        #   end
+        # end
+
   # Print only tenants that are over the age of 44
+          # tenants.each do |tenant|
+          #   if tenant[:age] > 44
+          #     puts tenant[:name]
+          #   end
+          # end
   # Print only tenants that have an apartment id of 1
+            # tenants.each do |tenant|
+            #   if tenant[:apartment_id] == 1
+            #     puts tenant[:name]
+            #   end
+            # end
   # Print all the tenants in order from youngest to oldest
+
+              # puts tenants.sort_by{|tenant| tenant[:age]}
+
+
   # Print the names of all the tenants alphabetically
+        # sorted_tenants = tenants.sort_by{|tenant| tenant[:name]}
+        # sorted_tenants.each do |tenant|
+        #   puts tenant[:name]
+        # end
 
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
