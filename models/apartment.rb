31a1bbd0f3316d4e00bd84f3a926d39c88714d51
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-07-20 18:42:59 -0400

    instances

--- models/apartment.rb
+++ models/apartment.rb
@@ -3,5 +3,8 @@ class Apartment
 
   def initialize(id, address, monthly_rent, square_feet)
     @id = id
-    
+    @address = address
+    @monthly_rent = monthly_rent
+    @square_feet = square_feet
+  end
 end
