f17017719a68d8dd85dc0764e3c5ac1157313f15
Marc Fournier <marc.fournier91@gmail.com>
2016-07-20 18:21:14 -0400

    MVP for part 2

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require_relative "data"
-apartments = data[:apartments]
-tenants = data[:tenants]
+@apartments = data[:apartments]
+@tenants = data[:tenants]
+
 
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
@@ -12,28 +13,58 @@ tenants = data[:tenants]
   #ID, address, monthly_rent, square_feet
   #ID,name,age,apartment_id
 
-# Use enumerables to -
-  # Print all the addresses for the apartments
-  apartments.each do |apartment|
-    puts apartments[:address]
-  end
-  # Print all the names for tenants
-  tenants.each do |tenant|
-    puts tenant[:name]
-  end
-  # Print only apartments that are less then 700 in rent
-  apartments.select{|apartment| apartment[:square_feet] < 700}
-  # Print only tenants that are over the age of 44
-  tenants.select{|tenant| tenant[:age] > 44}
-  # Print only tenants that have an apartment id of 1
-  tenants.select{|tenant| tenant[:id] == 1}
-  # Print all the tenants in order from youngest to oldest
-  tenants.sort_by!{|tenant| tenant[:age]}
-  tenants.each{|tenant| puts tenant}
-  # Print the names of all the tenants alphabetically
-  tenants.sort_by!{|tenants| tenant[:name]}
-  tenants.each{|tenant| puts tenant}  
+# # Use enumerables to -
+#   # Print all the addresses for the apartments
+#   apartments.each do |apartment|
+#     puts apartments[:address]
+#   end
+#   # Print all the names for tenants
+#   tenants.each do |tenant|
+#     puts tenant[:name]
+#   end
+#   # Print only apartments that are less then 700 in rent
+#   apartments.select{|apartment| apartment[:square_feet] < 700}
+#   # Print only tenants that are over the age of 44
+#   tenants.select{|tenant| tenant[:age] > 44}
+#   # Print only tenants that have an apartment id of 1
+#   tenants.select{|tenant| tenant[:id] == 1}
+#   # Print all the tenants in order from youngest to oldest
+#   tenants.sort_by!{|tenant| tenant[:age]}
+#   tenants.each{|tenant| puts tenant}
+#   # Print the names of all the tenants alphabetically
+#   tenants.sort_by!{|tenants| tenant[:name]}
+#   tenants.each{|tenant| puts tenant}
 
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
   # When printing all apartments, under each apartment print all of its tenants
+
+def startApp
+  puts ""
+  puts "Landlordy: the CLI app for landlords"
+  puts ""
+  puts "Enter 1 to view your apartment properties"
+  puts "Enter 2 to view your tenants"
+  return gets.chomp
+end
+
+def printApartments
+  puts ""
+  @apartments.each do |apartment|
+    puts "Apartment num: #{apartment[:id]}, Address : #{apartment[:address]}, rent: #{apartment[:rent]}, sq-ft : #{apartment[:square_feet]}"
+  end
+end
+
+def printTenants
+  puts ""
+  @tenants.each do |tenant|
+    puts "Tenant name : #{tenant[:name]}, age : #{tenant[:age]}"
+  end
+end
+
+userInput = startApp
+  if userInput == "1"
+    printApartments
+  elsif userInput == "2"
+    printTenants
+  end
