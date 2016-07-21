c261fed417c45d528f2a2e0e50adf5855f2fd594
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-07-20 20:39:28 -0400

    Updated Part 3, Step 2.

--- models/tenant.rb
+++ models/tenant.rb
@@ -1,3 +1,8 @@
 class Tenant
-  attr_accessor :tenants
+  attr_accessor :id, :name, :age, :apartment_id
+  def initialize (id, name, age, apartment_id)
+    @id = id
+    @name = name
+    @age = age
+    @apartment_id = apartment_id 
   end
