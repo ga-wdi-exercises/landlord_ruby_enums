49e71ff5680e058b26a511c4432f7a12b978db6e
Daniela Noir <daniela@danielanoir.com>
2016-07-20 20:44:08 -0400

    finished with hw

--- app.rb
+++ app.rb
@@ -13,36 +13,36 @@ ruby_tenants = tenants.map do |tenant|
   Tenant.new(tenant[:id], tenant[:name], tenant[:age], tenant[:apartment_id])
 end
 
-#PART ONE, SEE BELOW (HIDDEN)
+#PART ONE/FOUR, SEE BELOW (HIDDEN)
 #
-# # Use enumerables to -
-#
-# # Print all the addresses for the apartments
-# apartments.each do |apartment|
+# # # Use enumerables to -
+# #
+# # # Print all the addresses for the apartments
+# ruby_apartments.each do |apartment|
 #   puts apartment[:address]
 # end
-#
-# # Print all the names for tenants
-# tenants.each do |tenant|
+# #
+# # # Print all the names for tenants
+# ruby_tenants.each do |tenant|
 #   puts tenant[:name]
 # end
-# #
-# # # Print only apartments that are less then 700 in rent
-# apartments.each do |apartment|
+# # #
+# # # # Print only apartments that are less then 700 in rent
+# ruby_apartments.each do |apartment|
 #   if apartment[:square_feet] < 700
 #     puts apartment[:address]
 # end
 # end
-#
+# #
 # # # Print only tenants that are over the age of 44
-# tenants.each do |tenant|
+# ruby_tenants.each do |tenant|
 #   if tenant[:age] > 44
 #     puts tenant[:name]
 #   end
 # end
-# #
-# # # Print only tenants that have an apartment id of 1
-# tenants.each do |tenant|
+# # #
+# # # # Print only tenants that have an apartment id of 1
+# ruby_tenants.each do |tenant|
 #   if tenant[:id] == 1
 #     puts tenant[:name]
 #   end
@@ -50,13 +50,13 @@ end
 #
 # # # Print all the tenants in order from youngest to oldest
 # # NEED TO REVISIT
-# tenants.each do |tenant|
+# ruby_tenants.each do |tenant|
 #   puts tenant.sort[:age]
 # end
-# #
-# # # Print the names of all the tenants alphabetically
-# #NEED TO REVISIT
-# tenants.each do |tenant|
+# # #
+# # # # Print the names of all the tenants alphabetically
+# # #NEED TO REVISIT
+# ruby_tenants.each do |tenant|
 #   puts tenant.sort[:name]
 # end
 #
