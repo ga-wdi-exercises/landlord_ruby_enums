e6d0761525cd481abacaa0bc84d3c63ebeb84c7f
Tim Foley <thetimfoley@gmail.com>
2016-09-28 18:19:42 -0400

    finished enum practice

--- app.rb
+++ app.rb
@@ -12,27 +12,42 @@ tenants = data[:tenants]
 
 # Use enumerables to -
   # Print all the addresses for the apartments
-  # apartments.each do |apt|
-  #   p apt[:address]
-  # end
+    # apartments.each do |apt|
+    #   p apt[:address]
+    # end
   # Print all the names for tenants
-  # tenants.each do |t|
-  #   p t[:name]
-  # end
+    # tenants.each do |t|
+    #   p t[:name]
+    # end
   # Print only apartments that are less then 700 in rent
-  # cheap = apartments.select do |apt|
-  #   apt[:monthly_rent] < 700
-  # end
-  # puts cheap
+    # cheap = apartments.select do |apt|
+    #   apt[:monthly_rent] < 700
+    # end
+    # puts cheap
   # Print only tenants that are over the age of 44
-  tenants.each do |t|
-    if t[:age] > 44
-      p t[:name]
-    end
-  end
+    # tenants.each do |t|
+    #   if t[:age] > 44
+    #     p t[:name]
+    #   end
+    # end
   # Print only tenants that have an apartment id of 1
+    # apt_one = tenants.select do |t|
+    #   t[:apartment_id] == 1
+    # end
+    # puts apt_one
   # Print all the tenants in order from youngest to oldest
+    # this one was a bit harder. Ruby docs can be tough to read sometimes!!!!
+    # sorted_tenants = tenants.sort do |t1, t2|
+    #   t1[:age] <=> t2[:age]
+    # end
+    # puts sorted_tenants
   # Print the names of all the tenants alphabetically
+    # sorted_tenants = tenants.sort do |t1, t2|
+    #   t1[:name] <=> t2[:name]
+    # end
+    # sorted_tenants.each do |t|
+    #   p t[:name]
+    # end
 
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
