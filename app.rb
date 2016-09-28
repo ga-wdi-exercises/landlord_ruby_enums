0462cec5208e0f39a0f82f7dba3da4a60cb6330b
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-09-28 18:12:18 -0400

    sorted the array <from youngest to oldest>

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-require "pry"
+# require "pry"
 require_relative "data"
 require_relative "models/apartment"
 require_relative "models/tenant"
@@ -8,42 +8,54 @@ tenants = data[:tenants]
 
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
-  There are 2 arrays with 2 types of hashes
+  # There are 2 arrays with 2 types of hashes
   # What are the properties for each of the two types of hashes
-  1.apartments have id, address, monthly_rent,square_feet
-  2.tenants have id, name, age, apartment_id
+  # 1.apartments have id, address, monthly_rent,square_feet
+  # 2.tenants have id, name, age, apartment_id
 
 # Use enumerables to -
   # Print all the addresses for the apartments
   addresses = apartments.map do |apartment|
         apartment[:address]
    end
-   puts addresses
+    addresses
 
   # Print all the names for tenants
   names = tenants.map do |tenant|
       tenant[:name]
   end
-  puts names
+   names
 
   # Print only apartments that are less then 700 in rent
   less_expencive_apartments = apartments.select do |apartment|
     apartment[:monthly_rent] < 700
   end
-  puts less_expencive_apartments
+  less_expencive_apartments
   # Print only tenants that are over the age of 44
   tenants_over_forty = tenants.select do |tenant|
     tenant[:age] > 40
   end
-  puts tenants_over_forty
+  tenants_over_forty
 
   # Print only tenants that have an apartment id of 1
+  tenants.each do |tenant|
+      if tenant[:apartment_id] == 1
+         tenant
+      end
+  end
+
   # Print all the tenants in order from youngest to oldest
+  tenants_by_age = tenants.sort_by do |tenant|
+    tenant[:age]
+  end
+  puts tenants_by_age
+
+
   # Print the names of all the tenants alphabetically
 
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
   # When printing all apartments, under each apartment print all of its tenants
-binding.pry
+# binding.pry
 
-puts "end of the code!"
+# puts "end of the code!"
