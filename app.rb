6f73b96c4a5f633cc896b36953ed73828c01f17c
Nick Oki <nick.oki@live.com>
2016-09-28 18:17:04 -0400

    Part 2 Silver

--- app.rb
+++ app.rb
@@ -66,27 +66,106 @@ tenants = data[:tenants]
 
 
 	# When printing all apartments, under each apartment print all of its tenants
-	def print_apartments apartments, tenants
-		apartments.each do |a|
-			t = tenants.select { |t| t[:apartment_id] == a[:id] }
-			t_array = []
-			t.each { |t| t_array << t[:name]}
-			puts "#{a[:address]}, #{t_array}"
-		end
+	# def apartments_detail apartments, tenants
+	# 	apartments.each do |a|
+	# 		t = tenants.select { |t| t[:apartment_id] == a[:id] }
+	# 		t_array = []
+	# 		t.each { |t| t_array << t[:name]}
+	# 		puts "#{a[:address]}, #{t_array}"
+	# 	end
+	# end
+
+
+
+# Print apartment list
+def apartments_list apartments
+	apartments.each_with_index do |a, i|
+		puts "\n##{i + 1}: #{a[:address]}"
 	end
+
+end
+
+# Print apartment details
+def apartments_detail apartments, tenants
+	apartments.each_with_index do |a, i|
+		t = tenants.select { |t| t[:apartment_id] == a[:id] }
+		puts "\n##{i + 1}: #{a[:address]}\nRent: $#{a[:monthly_rent]}\nSize: #{a[:square_feet]}\nTenants:"
+		t.each { |t| puts " - #{t[:name]}" }
+	end
+
+end
+
+
+
+# Apartment options
+def apartment_options apartments, tenants
+	print "\n=== Apartment Sub-Menu ===\nEnter [1] for apartment list\nEnter [2] for apartment details\n>>> "
+	input = gets.chomp
+
+	# Process input
+	if input == '1'
+		apartments_list apartments
+	elsif input == '2'
+		apartments_detail apartments, tenants
+	end
+end
+
+
+
+# Print tenant list
+def tenants_list tenants
+	tenants.each_with_index do |t, i|
+		puts "\n##{i + 1}: #{t[:name]}"
+	end
+end
+
+# Print tenant details
+def tenants_detail apartments, tenants
+	tenants.each_with_index do |t, i|
+		a = apartments.select { |a| a[:id] == t[:apartment_id] }
+		puts "\n##{i + 1}: #{t[:name]}\nAge: #{t[:age]}\nAddress: #{a[0][:address]}"
+	end
+end
+
+# Tenant lookup
+def tenant_lookup apartments, tenants
+	puts "\nEnter tenant's name"
+	print ">>> "
+	name = gets.chomp
+	name = tenants.select { |t| t[:name].downcase == name.downcase }
+	a = apartments.select { |a| a[:id] == name[0][:apartment_id] }
+	puts a[0][:address]
+end
+
+# Tenant options
+def tenant_options apartments, tenants
+	print "\n=== Tenant Sub-Menu ===\nEnter [1] for tenant list\nEnter [2] for tenant details\nEnter [3] to look up a tenant's address\n>>> "
+	input = gets.chomp
+
+	# Process input
+	if input == '1'
+		tenants_list tenants
+	elsif input == '2'
+		tenants_detail apartments, tenants
+	elsif input == '3'
+		tenant_lookup apartments, tenants
+	end
+end
 
 
 
 # Begin Program
-puts "=== Landlord Main Menu ==="
-puts "Enter [1] for apartment overview"
-puts "Enter [2] for tenant overview"
-print ">>> "
-input = gets.chomp
+def start_program apartments, tenants
+	print "\n=== Landlord Main Menu ===\nEnter [1] for apartments\nEnter [2] for tenants\n>>> "
+	input = gets.chomp
 
-# Process input
-if input == '1'
-	print_apartments apartments, tenants
-elsif input == '2'
-	print_tenants tenants, apartments
+	# Process input
+	if input == '1'
+		apartment_options apartments, tenants
+	elsif input == '2'
+		tenant_options apartments, tenants
+	end
 end
+
+
+start_program apartments, tenants
