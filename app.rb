b0c9e9cee313c54c4bddc907db19ad9745792c2b
Khoi Le <khoileee89@gmail.com>
2016-09-28 18:54:30 -0400

    initial part 2

--- app.rb
+++ app.rb
@@ -10,51 +10,60 @@ tenants = data[:tenants]
   # What are the properties for each of the two types of hashes
 
 # Use enumerables to -
-  # Print all the addresses for the apartments
-  address = apartments.each do |hash|
-    puts hash[:address]
-  end
-  # Print all the names for tenants
-name = tenants.each do |hash|
-  puts hash[:name]
-end
-  # Print only apartments that are less then 700 in rent
-less_than700 = apartments.each do |hash|
-  if(hash[:monthly_rent]<700)
-    p hash[:monthly_rent]
-  end
+#   Print all the addresses for the apartments
+#   address = apartments.each do |hash|
+#     puts hash[:address]
+#   end
+#   # Print all the names for tenants
+# name = tenants.each do |hash|
+#   puts hash[:name]
+# end
+#   # Print only apartments that are less then 700 in rent
+# less_than700 = apartments.each do |hash|
+#   if(hash[:monthly_rent]<700)
+#     p hash[:monthly_rent]
+#   end
+# end
+#
+#   # Print only tenants that are over the age of 44
+#   tenants_over44 = tenants.each do |hash|
+#     if(hash[:age] > 44)
+#       p hash[:name]
+#     end
+#   end
+#   # Print only tenants that have an apartment id of 1
+#   tenant_id1 = tenants.each do |hash|
+#     if(hash[:id] ==1)
+#     p hash[:id]
+#     end
+# end
+#   # Print all the tenants in order from youngest to oldest
+# # index = 0
+# # while index < apartments.length
+# #   tenants.each do |hash|
+# #     puts data[:age]
+# #   index +=1
+# # end
+# # # p tenants
+# # end
+# young_to_old = tenants.sort_by do |tenant|
+#    tenant[:age]
+# end
+# puts young_to_old
+#   # Print the names of all the tenants alphabetically
+# name_alphabetically = tenants.sort_by do |name|
+#   name[:name]
+# end
+# puts name_alphabetically
+puts "Enter a number Between 1-10"
+user_input = gets.chomp.to_i
+if user_input == 1
+  p apartments
+else
+  "Pick another numnber"
 end
 
-  # Print only tenants that are over the age of 44
-  tenants_over44 = tenants.each do |hash|
-    if(hash[:age] > 44)
-      p hash[:name]
-    end
-  end
-  # Print only tenants that have an apartment id of 1
-  tenant_id1 = tenants.each do |hash|
-    if(hash[:id] ==1)
-    p hash[:id]
-    end
-end
-  # Print all the tenants in order from youngest to oldest
-# index = 0
-# while index < apartments.length
-#   tenants.each do |hash|
-#     puts data[:age]
-#   index +=1
-# end
-# # p tenants
-# end
-young_to_old = tenants.sort_by do |tenant|
-   tenant[:age]
-end
-puts young_to_old
-  # Print the names of all the tenants alphabetically
-name_alphabetically = tenants.sort_by do |name|
-  name[:name]
-end
-puts name_alphabetically
+
 
 
 
