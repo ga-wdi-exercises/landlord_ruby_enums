95cbb79689c2e13bf4e3f2735a7f61ea471a611b
Andrew Whitley <awhitley1233@gmail.com>
2016-09-28 20:56:19 -0400

    finished MVP

--- app.rb
+++ app.rb
@@ -16,13 +16,13 @@ end
 tenants.each do |ten|
   ruby_tenants << Tenant.new(ten[:id],ten[:name],ten[:age],ten[:apartment_id])
 end
-
+#
 puts "Press: 1 to view apartments; 2 to view tenants"
 choice = gets.chomp.to_i
 if choice == 1
-  puts data[:apartments]
+  puts ruby_apartments
 elsif choice == 2
-  puts data[:tenants]
+  puts ruby_tenants
 end
 
 
@@ -30,22 +30,22 @@ end
 
 
 
-# cheap = apartments.map { |apartment| apartment[:monthly_rent]}
-# tenants.each { |tenant| puts tenant[:name]}
+# ruby_apartments.each { |apartment| puts apartment.monthly_rent}
+# ruby_tenants.each { |tenant| puts tenant.name}
 
-# cheap_apartments = apartments.select { |apartment| apartment[:monthly_rent] < 700 }
+# cheap_apartments = ruby_apartments.select { |apartment| apartment.monthly_rent < 700 }
 # puts cheap_apartments
 
-# old_tenants = tenants.select { |tenant| tenant[:age] > 44}
+# old_tenants = ruby_tenants.select { |tenant| tenant.age > 44}
 # puts old_tenants
-
-# tenant_1 = tenants.select { |tenant| tenant[:apartment_id] == 1}
+#
+# tenant_1 = ruby_tenants.select { |tenant| tenant.apartment_id == 1}
 # puts tenant_1
-
-# tenants_by_age = tenants.sort_by { |tenant| tenant[:age]}
+#
+# tenants_by_age = ruby_tenants.sort_by { |tenant| tenant.age}
 # puts tenants_by_age
-
-# tenants_by_name = tenants.sort_by { |tenant| tenant[:name]}
+#
+# tenants_by_name = ruby_tenants.sort_by { |tenant| tenant.name}
 # puts tenants_by_name
 
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
