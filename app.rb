67d40771ba901d1f9936b1470e162335705b64d1
Kevin Mahoney <kjmahoney13@gmail.com>
2016-09-28 20:36:03 -0400

    ready for submissiono

--- app.rb
+++ app.rb
@@ -11,20 +11,7 @@ tenants = data[:tenants]
 ruby_apartments = []
 ruby_tenants = []
 
-#app interface-----------------------------
-# puts "Hi! press 1 for tenant data. Press 2 for apartment data"
-#
-# userInput = gets.chomp
-#
-# if userInput == 1
-#   puts data[:apartments]
-# elsif userInput == 2
-#   puts data[:tenants]
-# else
-#   puts "command not recognized, please try again"
-# end
-
-#Part3___________
+#PART 3_______________________________
 
 apartments.each do |apt|
   ruby_apartments << Apartment.new(apt[:id],apt[:address],apt[:monthly_rent],apt[:square_feet])
@@ -36,70 +23,41 @@ tenants.each do |tenant|
 end
 
 
-binding.pry
+#PART 2-----------------------------
+puts "Hi! press 1 for tenant data. Press 2 for apartment data"
 
-#Part 1______________________________
+userInput = gets.chomp.to_i
 
-# index = 0
-# while index < apartments.length
-#   puts apartments[index][:address]
-#   index += 1
-# end
-
-ruby_apartments.each do |apt|
-  puts apt[:address]
+if userInput == 1
+  puts ruby_tenants
+elsif userInput == 2
+  puts ruby_apartments
+else
+  puts "command not recognized, please try again"
 end
 
 
-# index = 0
-# while index < tenants.length
-#   puts tenants[index][:name]
-#   index += 1
-# end
+#Part 1______________________________________
 
-# index = 0
-# while index < apartments.length
-#   if apartments[index][:monthly_rent] < 700
-#     puts apartments[index][:id]
-#     index += 1
-#   else
-#    index += 1
-#  end
-# end
 
-# index = 0
-# while index < tenants.length
-#   if tenants[index][:age] > 44
-#     puts tenants[index][:name]
-#     index += 1
-#   else
-#    index += 1
-#  end
-# end
+# ruby_apartments.each {|apt| puts apt.address}
 
-# index = 0
-# while index < tenants.length
-#   if tenants[index][:apartment_id] == 1
-#     puts tenants[index][:name]
-#     index += 1
-#   else
-#     index += 1
-#  end
-# end
-#
-# index = 0
-# while index < tenants.length
-#   if tenants[index][:apartment_id] == 1
-#     puts tenants[index][:name]
-#     index += 1
-#   else
-#     index += 1
-#  end
-# end
+# ruby_tenants.each {|tenant| puts tenant.name}
 
-# puts tenants.sort_by{|tenant| tenant[:age]}
+# cheap_apartments = ruby_apartments.each {|apt| puts apt.monthly_rent < 700}
+# puts cheap_apartments
 
-# puts tenants.sort_by{|tenant| tenant[:name]}
+# old_tenants = ruby_tenants.each {|tenant| tenant.age > 44}
+# puts old_tenants
+
+# apartment_one = ruby_tenants.each {|tenant| tenant.apartment_id == 1}
+# puts apartment_one
+
+# puts ruby_tenants.sort_by{|tenant| tenant.age}
+
+# puts ruby_tenants.sort_by{|tenant| tenant.name}
+
+binding.pry
 
 
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
@@ -118,3 +76,47 @@ end
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
   # When printing all apartments, under each apartment print all of its tenants
+
+
+
+
+
+#NOTES___________________________________________
+  #_______old while loop code from part 1  *deleted loop from step 1
+
+  # index = 0
+  # while index < tenants.length
+  #   puts tenants[index][:name]
+  #   index += 1
+  # end
+
+  # index = 0
+  # while index < apartments.length
+  #   if apartments[index][:monthly_rent] < 700
+  #     puts apartments[index][:id]
+  #     index += 1
+  #   else
+  #    index += 1
+  #  end
+  # end
+
+  # index = 0
+  # while index < tenants.length
+  #   if tenants[index][:age] > 44
+  #     puts tenants[index][:name]
+  #     index += 1
+  #   else
+  #    index += 1
+  #  end
+  # end
+
+  #
+  # index = 0
+  # while index < tenants.length
+  #   if tenants[index][:apartment_id] == 1
+  #     puts tenants[index][:name]
+  #     index += 1
+  #   else
+  #     index += 1
+  #  end
+  # end
