66891a7b9d6a0cc5e9cb231acfa5413e0bda5ec4
Chris Jaffier <chrisd@Chriss-MacBook-Pro.local>
2016-09-29 08:51:20 -0400

    commented on data types

--- data.rb
+++ data.rb
@@ -21,7 +21,7 @@ def data
     {:id=>19, :address=>"72108 Leuschke Greens", :monthly_rent=>661, :square_feet=>967},
     {:id=>20, :address=>"4705 Rosenbaum Ville", :monthly_rent=>993, :square_feet=>1168}
   ]
-
+#creates and array of appartments
   tenants = [
     {:id=>1, :name=>"Ms. Maryse Farrell", :age=>87, :apartment_id=>8},
     {:id=>2, :name=>"Maci Hickle II", :age=>28, :apartment_id=>1},
@@ -76,3 +76,4 @@ def data
   ]
   return {apartments:apartments, tenants:tenants}
 end
+#creates and array of tenants
