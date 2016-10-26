665d8edd9c3464362080a6d6b7bf100d0e6eddcd
Robert Zehnder <robzcodes@gmail.com>
2016-10-26 10:19:37 -0400

    Adds silver and gold steps before conversion to classes

---
+++ apartment.rb
@@ -0,0 +1,6 @@
+class Apartment
+  def initialize(id,address,monthly_rent,square_feet)
+    @id = id
+    @address = address
+  end
+end
--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative "data"
+#require 'pry'
 apartments = data[:apartments]
 tenants = data[:tenants]
 
@@ -18,68 +19,188 @@ tenants = data[:tenants]
 #   WARNING: puts commented out, so nothing will print
 # -------------------------------------------------------
 
+# OLD COMMENTED CODE BEGINS HERE
   # Print all the addresses for the apartments
-  apartments.each do |apt|
-    #puts apt[:address]
-  end
-
-  # Print all the names for tenants
-  tenants.each do |tenant|
-    #puts tenant[:name]
-  end
-
-  # Print only apartments that are less then 700 in rent
-  apartments.each do |apt|
-    if apt[:monthly_rent] < 700
-      #puts apt[:monthly_rent]
-    end
-  end
-
-  # Print only tenants that are over the age of 44
-  tenants.each do |tenant|
-    if tenant[:age] > 44
-      #puts tenant[:age]
-    end
-  end
-
-  # Print only tenants that have an apartment id of 1
-  tenants.each do |tenant|
-    if tenant[:apartment_id] == 1
-      #puts tenant[:apartment_id]
-    end
-  end
-
-  # Print all the tenants in order from youngest to oldest
-  newArray = tenants.sort_by {|tenant| tenant[:age]}
-  newArray.each do |tenant|
-    #puts tenant[:age]
-  end
-
-  # Print the names of all the tenants alphabetically
-  newArray = tenants.sort_by {|tenant| tenant[:name]}
-  newArray.each do |tenant|
-    #puts tenant[:name]
-  end
-
-  ## More challenging
-
-  # When printing tenants also print out the address that the tenant resides in.
-  tenants.each do |tenant|
-    tenantApt = tenant[:apartment_id]
-    apartments.each do |apt|
-      if apt[:id] == tenantApt
-        #puts "#{tenant[:name]} lives at: #{apt[:address]}"
+    def addresses(aptArr)
+      aptArr.each do |apt|
+        #puts apt[:address]
       end
     end
-  end
-
-  # When printing all apartments, under each apartment print all of its tenants
-
-  apartments.each do |apt|
-    aptID = apt[:id]
-    tenants.each do |tenant|
-      if tenant[:apartment_id] == aptID
-        puts "#{tenant[:name]} lives at #{apt[:id]}"
+      # Print all the names for tenants
+    def addresses(tenantArr)
+      tenantArr.each do |tenant|
+        #puts tenant[:name]
       end
     end
-  end
+      # Print only apartments that are less then 700 in rent
+    def addresses(aptArr)
+      aptArr.each do |apt|
+        if apt[:monthly_rent] < 700
+          #puts apt[:monthly_rent]
+        end
+      end
+    end
+      # Print only tenants that are over the age of 44
+    def addresses(tenantArr)
+      tenantArr.each do |tenant|
+        if tenant[:age] > 44
+          #puts tenant[:age]
+        end
+      end
+    end
+      # Print only tenants that have an apartment id of 1
+    def addresses(tenantArr)
+      tenantArr.each do |tenant|
+        if tenant[:apartment_id] == 1
+          #puts tenant[:apartment_id]
+        end
+      end
+    end
+      # Print all the tenants in order from youngest to oldest
+    def addresses(tenantArr)
+      newArray = tenantArr.sort_by {|tenant| tenant[:age]}
+      newArray.each do |tenant|
+        #puts tenant[:age]
+      end
+    end
+      # Print the names of all the tenants alphabetically
+    def addresses(tenantArr)
+      newArray = tenantArr.sort_by {|tenant| tenant[:name]}
+      newArray.each do |tenant|
+        #puts tenant[:name]
+      end
+    end
+      ## More challenging
+
+      # When printing tenants also print out the address that the tenant resides in.
+    def tenantHome(aptArr,tenantArr)
+      tenantArr.each do |tenant|
+        tenantApt = tenant[:apartment_id]
+        aptArr.each do |apt|
+          if apt[:id] == tenantApt
+            puts "#{tenant[:name]} lives at: #{apt[:address]}"
+          end
+        end
+      end
+    end
+      # When printing all apartments, under each apartment print all of its tenants
+
+    def aptTenants(aptArr,tenantArr)
+      aptArr.each do |apt|
+        aptID = apt[:id]
+        tenantArr.each do |tenant|
+          if tenant[:apartment_id] == aptID
+            puts "#{tenant[:name]} lives at #{apt[:id]}"
+          end
+        end
+      end
+    end
+      # apartments: id,address,monthly_rent,square_feet
+      # tenants: id,name,age,apartment_id
+
+    def removeTenant(tenantArr,id)
+      tenantArr.each do |tenant|
+        if tenant[:id] == id
+          tenant[:apartment_id] = 0
+        end
+      end
+    end
+
+
+    #Add new apartment
+    def addApt(id,address,monthly_rent,square_feet)
+      hash = {:id => id}
+      hash[:address] = address
+      hash[:monthly_rent] = monthly_rent
+      hash[:square_feet] = square_feet
+      return hash
+    end
+    #Add new tenant
+    def addTenant(id,name,age,apartment_id)
+      hash = {:id => id}
+      hash[:name] = name
+      hash[:age] = age
+      hash[:apartment_id] = apartment_id
+      return hash
+    end
+
+    answer = 0
+    until answer == 99
+      puts " ",
+      "Welcome Bob! Here are the following options and commands:",
+      "1 - View all of your apartments",
+      "2 - View all of your tenants",
+      "3 - View all the apartments with their tenants",
+      "4 - View tenant's home address based on name",
+      "5 - Remove tenant from property",
+      "6 - Add Apartment",
+      "7 - Add Tenant",
+      "99 - Exit the program",
+      " "
+
+      answer = gets.to_i
+      puts ' '
+      if answer == 1
+
+          apartments.each do |apt|
+            puts "(ID) #{apt[:id]}   (Address) #{apt[:address]}   (Rent) #{apt[:monthly_rent]}   (Square Feet) #{apt[:square_feet]}" #id,address,monthly_rent,square_feet
+          end
+
+      elsif answer == 2
+
+          tenants.each do |apt| # tenants: id,name,age,apartment_id
+            puts "(ID) #{apt[:id]}   (Name) #{apt[:name]}    (Age) #{apt[:age]}    (AptID) #{apt[:apartment_id]}" #id,address,monthly_rent,square_feet
+          end
+
+      elsif answer == 3
+
+          aptTenants(apartments,tenants)
+
+      elsif answer == 4
+
+          tenantHome(apartments,tenants)
+
+      elsif answer == 5
+
+          puts ' '
+          puts "Input Tenant ID"
+          id = gets.to_i
+          puts ' '
+          removeTenant(tenants,id)
+          #add in a message of who you removed
+
+      elsif answer == 6
+
+          puts "Enter the id: "
+          id = gets.to_i
+          puts "Enter the address: "
+          address = gets.chomp
+          puts "Enter the rent amount: "
+          monthly_rent = gets.to_i
+          puts "Enter the sq feet: "
+          square_feet = gets.to_i
+          hash = addApt(id,address,monthly_rent,square_feet)
+          apartments.push(hash)
+          puts " "
+
+      elsif answer == 7 # tenants: id,name,age,apartment_id
+
+          puts "Enter the id: "
+          id = gets.to_i
+          puts "Enter the name: "
+          name = gets.chomp
+          puts "Enter the age: "
+          age = gets.to_i
+          puts "Enter the apartment_id: "
+          apartment_id = gets.to_i
+          hash = addTenant(id,name,age,apartment_id)
+          tenants.push(hash)
+          puts " "
+
+      end
+    end
+
+
+
+
+#binding.pry
