cd475751d794f1ebece94bfc0bf97c19334a1065
Angel Valant <angelgithub@gmail.com>
2016-10-26 19:43:00 -0400

    updates app.rb

--- app.rb
+++ app.rb
@@ -12,19 +12,49 @@ tenants = data[:tenants]
 # --> The tenants hash contains information that identifys a tenant by name, a record id, their age and which apartment they are assigned -- the tenant is the object.
 
 
-# Use enumerables to -
-  # Print all the addresses for the apartments
-  aptaddy = apartments.each do {|apartment| apartment[:address]}
-    puts aptaddy
-  end
+# # Use enumerables to -
+#   # Print all the addresses for the apartments
+#   apartments.each {|apartment| puts apartment[:address]}
+#
+#   # Print all the names for tenants
+#   tenants.each {|tenant| puts tenant[:name]}
+#
+#   # Print only apartments that are less then 700 in rent
+#   cheapapartment = apartments.select {|apartment|  apartment[:monthly_rent] < 700}
+#     puts cheapapartment
+#
+#
+#   # Print only tenants that are over the age of 44
+#   olderresidents = tenants.select {|tenant| tenant[:age] > 44}
+#     puts olderresidents
+#
+#   # Print only tenants that have an apartment id of 1
+#   apartmentid = tenants.select {|tenant| tenant[:apartment_id] == 1}
+#     puts apartmentid
+#
+#   # Print all the tenants in order from youngest to oldest
+#   numorder = tenants.sort_by {|tenant| tenant[:age]}
+#     puts numorder
+#
+#   # Print the names of all the tenants alphabetically
+#   alphaorder = tenants.sort_by {|tenant| tenant[:name]}
+#     puts alphaorder
 
-  # Print all the names for tenants
-  # Print only apartments that are less then 700 in rent
-  # Print only tenants that are over the age of 44
-  # Print only tenants that have an apartment id of 1
-  # Print all the tenants in order from youngest to oldest
-  # Print the names of all the tenants alphabetically
 
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
+
+  # tenants.each do |tenant|
+  #   puts tenant[:name]
+  #   apartment = apartments.find {|apartment| tenant[:apartment_id] == apartment[:id]}
+  #   puts apartment[:address]
+  # end
+
   # When printing all apartments, under each apartment print all of its tenants
+  apartments.each do |apartment|
+    puts apartment[:address]
+    tenant = tenants.find {|tenant| tenant[:apartment_id] == apartment[:id]}
+    tenant.each do |tenant|
+      puts tenant[:name]
+    end
+  end
