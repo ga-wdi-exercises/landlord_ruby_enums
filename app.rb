50091c0585e55d148739f96cffe766dc2cb1b14c
robert <robert.k25@gmail.com>
2016-10-26 22:36:45 -0400

    adds comment answers for data.rb and adds print addresses

--- app.rb
+++ app.rb
@@ -2,13 +2,24 @@ require_relative "data"
 apartments = data[:apartments]
 tenants = data[:tenants]
 
-# First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
-  # Explain how the data is structured
-  # What are the properties for each of the two types of hashes
+
 
 # Use enumerables to -
+
+
+
   # Print all the addresses for the apartments
+  def print_addresses(apartments)
+    addresses = apartments.map { |apartment| aparment[:address]  }
+    addresses.each { |address| puts address }
+  end
+
   # Print all the names for tenants
+  
+
+
+
+
   # Print only apartments that are less then 700 in rent
   # Print only tenants that are over the age of 44
   # Print only tenants that have an apartment id of 1
@@ -18,3 +29,6 @@ tenants = data[:tenants]
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
   # When printing all apartments, under each apartment print all of its tenants
+
+
+print_addresses(apartments)
