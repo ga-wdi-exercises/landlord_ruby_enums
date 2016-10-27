b84ffed8eaaf17fba46f6d2ee29a74161a3c1f30
robert <robert.k25@gmail.com>
2016-10-27 09:31:21 -0400

    adds part 2 mvp and adds files for future models

--- app.rb
+++ app.rb
@@ -4,40 +4,55 @@ tenants = data[:tenants]
 
 # Use enumerables to -
   # Print all the addresses for the apartments
-  def print_addresses(apartments)
-    addresses = apartments.map { |apartment| apartment[:address]  }
-    addresses.each { |address| puts address }
+  # def print_addresses(apartments)
+  #   addresses = apartments.map { |apartment| apartment[:address]  }
+  #   addresses.each { |address| puts address }
+  # end
+  #
+  # # Print all the names for tenants
+  # def print_tenant_names(tenants)
+  #   names = tenants.map { |tenant| tenant[:name]  }
+  #   names.each { |name| puts name }
+  # end
+  #
+  # # Print only apartments that are less then 700 in rent
+  # def print_apartments_under(apartments, price)
+  #   deals =  apartments.select { |apartment| apartment[:monthly_rent] < price }
+  #   deals.each { |deal| puts deal }
+  # end
+  #
+  # # Print only tenants that are over the age of 44
+  # def print_tenants_older_than(tenants, age)
+  #   #Kappa
+  #   soylent = tenants.select { |tenant| tenant[:age] > age }
+  #   soylent.each { |person| puts person  }
+  # end
+  # # Print only tenants that have an apartment id of 1
+  # def tenants_by_apartment_id(tenants, id)
+  #   puts tenants.select { |tenant| tenant[:apartment_id] == id  }
+  # end
+  # # Print all the tenants in order from youngest to oldest
+  # def tenants_by_age(tenants)
+  #   puts tenants.sort_by { |tenant| tenant[:age]  }
+  # end
+  # # Print the names of all the tenants alphabetically
+  # def tenants_by_name(tenants)
+  #   puts tenants.sort_by { |tenant| tenant[:name]  }
+  # end
+
+  ##Part 2 methods
+  def show_all_apartments(apartments)
+    puts "Apartments"
+    apartments.each { |apartment|
+      puts "Unit: #{apartment[:id]}, Address: #{apartment[:address]}, Size: #{apartment[:square_feet]}, rent $#{apartment[:monthly_rent]}"
+    }
   end
 
-  # Print all the names for tenants
-  def print_tenant_names(tenants)
-    names = tenants.map { |tenant| tenant[:name]  }
-    names.each { |name| puts name }
-  end
-
-  # Print only apartments that are less then 700 in rent
-  def print_apartments_under(apartments, price)
-    deals =  apartments.select { |apartment| apartment[:monthly_rent] < price }
-    deals.each { |deal| puts deal }
-  end
-
-  # Print only tenants that are over the age of 44
-  def print_tenants_older_than(tenants, age)
-    #Kappa
-    soylent = tenants.select { |tenant| tenant[:age] > age }
-    soylent.each { |person| puts person  }
-  end
-  # Print only tenants that have an apartment id of 1
-  def tenants_by_apartment_id(tenants, id)
-    puts tenants.select { |tenant| tenant[:apartment_id] == id  }
-  end
-  # Print all the tenants in order from youngest to oldest
-  def tenants_by_age(tenants)
-    puts tenants.sort_by { |tenant| tenant[:age]  }
-  end
-  # Print the names of all the tenants alphabetically
-  def tenants_by_name(tenants)
-    puts tenants.sort_by { |tenant| tenant[:name]  }
+  def show_all_tenants(tenants)
+    puts "Tenants"
+    tenants.each { |tenant|
+      puts "Name: #{tenant[:name]} Age: #{tenant[:age]} Unit: #{tenant[:apartment_id]}"
+    }
   end
 
   ## More challenging
@@ -45,10 +60,27 @@ tenants = data[:tenants]
   # When printing all apartments, under each apartment print all of its tenants
 
 
-print_addresses(apartments)
-print_tenant_names(tenants)
-print_apartments_under(apartments, 700)
-print_tenants_older_than(tenants, 44)
-tenants_by_apartment_id(tenants, 1)
-tenants_by_age(tenants)
-tenants_by_name(tenants)
+# print_addresses(apartments)
+# print_tenant_names(tenants)
+# print_apartments_under(apartments, 700)
+# print_tenants_older_than(tenants, 44)
+# tenants_by_apartment_id(tenants, 1)
+# tenants_by_age(tenants)
+# tenants_by_name(tenants)
+
+loop {
+  puts "Welcome, Bob. Property Management Menu"
+  puts "1 - View All Apartments"
+  puts "2 - View All Tenets"
+  puts "0 - Exit the Program"
+  option = gets.chomp.to_i
+  break if option == 0
+  if option == 1
+    show_all_apartments(apartments)
+  elsif option == 2
+    show_all_tenants(tenants)
+  else
+    puts "Unrecognized Option: #{option}"
+  end
+
+}
