87af192c5bacd1b51a84621c632133cfb17b98de
Angel Valant <angelgithub@gmail.com>
2016-10-26 21:04:46 -0400

    updates app.rb

--- app.rb
+++ app.rb
@@ -13,42 +13,79 @@ tenants = data[:tenants]
 
 # Use enumerables to -
 # Print all the addresses for the apartments
-  apartments.each {|apartment| puts apartment[:address]}
+  # apartments.each {|apartment| puts apartment[:address]}
 
 # Print all the names for tenants
-  tenants.each {|tenant| puts tenant[:name]}
+  # tenants.each {|tenant| puts tenant[:name]}
 
 # Print only apartments that are less then 700 in rent
-  cheapapartment = apartments.select {|apartment|  apartment[:monthly_rent] < 700}
-    puts cheapapartment
+  # cheapapartment = apartments.select {|apartment|  apartment[:monthly_rent] < 700}
+  #   puts cheapapartment
 
 
 # Print only tenants that are over the age of 44
-  olderresidents = tenants.select {|tenant| tenant[:age] > 44}
-    puts olderresidents
+  # olderresidents = tenants.select {|tenant| tenant[:age] > 44}
+  #   puts olderresidents
 
 # Print only tenants that have an apartment id of 1
-  apartmentid = tenants.select {|tenant| tenant[:apartment_id] == 1}
-    puts apartmentid
+  # apartmentid = tenants.select {|tenant| tenant[:apartment_id] == 1}
+  #   puts apartmentid
 
 # Print all the tenants in order from youngest to oldest
-  numorder = tenants.sort_by {|tenant| tenant[:age]}
-    puts numorder
+  # numorder = tenants.sort_by {|tenant| tenant[:age]}
+  #   puts numorder
 
 # Print the names of all the tenants alphabetically
-  alphaorder = tenants.sort_by {|tenant| tenant[:name]}
-    puts alphaorder
+  # alphaorder = tenants.sort_by {|tenant| tenant[:name]}
+  #   puts alphaorder
 
 
 # More challenging
 # When printing tenants also print out the address that the tenant resides in.
-  tenants.each do |tenant|
-    puts tenant[:name]
-    apartment = apartments.find {|apartment| tenant[:apartment_id] == apartment[:id]}
-    puts apartment[:address]
-  end
+  # tenants.each do |tenant|
+  #   puts tenant[:name]
+  #   apartment = apartments.find {|apartment| tenant[:apartment_id] == apartment[:id]}
+  #   puts apartment[:address]
+  # end
 
 # When printing all apartments, under each apartment print all of its tenants
+  # apartments.each do |apartment|
+  #   puts apartment[:address]
+  #   tenant = tenants.select {|tenant| tenant[:apartment_id] == apartment[:id]}
+  #   tenant.each do |tenant|
+  #     puts tenant[:name]
+  #   end
+  # end
+
+# MVP
+puts "Buffering..\n
+-------------------------------\n
+Apartment Management Application\n
+-------------------------------\n
+Select one of the following options:\n
+1 -> View All Apartments\n
+2 -> View All Tenants\n
+3 -> View All Occupied Apartments\n
+4 -> View Occupied Apartments Based on Tenant Name\n
+5 -> Remove A Tenant\n
+6 -> Add A Tenant to Apartment\n
+7 -> New Apartment\n
+8 -> Create New Tenant\n"
+
+user_input = gets.chomp.to_i
+
+puts "You have selected option: #{user_input}"
+
+if user_input == 1
+
+  apartments.each {|apartment| puts apartment}
+
+elsif user_input == 2
+
+  tenants.each {|tenant| puts tenant}
+
+elsif user_input== 3
+
   apartments.each do |apartment|
     puts apartment[:address]
     tenant = tenants.select {|tenant| tenant[:apartment_id] == apartment[:id]}
@@ -56,3 +93,52 @@ tenants = data[:tenants]
       puts tenant[:name]
     end
   end
+
+elsif user_input == 4
+
+  tenants.each do |tenant|
+    puts "#{tenant[:name]} Occupies Apartment: "
+    apartment = apartments.find {|apartment| tenant[:apartment_id] == apartment[:id]}
+    puts apartment[:address]
+  end
+
+elsif user_input == 5
+  # I need to write a function that removes a tenant association or tenant from system?
+
+  def method_name
+
+  end
+
+elsif user_input == 6
+  # I need to write a function that adds a tenant association to an apartment
+
+  def method_name
+
+  end
+
+elsif user_input == 7
+  # I need to write a function that creates a new apartment and pushes it into the existing hash structure.
+  def ()
+    {:id=>,
+      :address=>,
+      :monthly_rent=>,
+      :square_feet=>},
+
+    end
+
+
+
+
+  else user_input == 8
+    # I need to write a function that creates a new apartment and pushes it into the exisitng hash
+
+    def()
+      {:id=>,
+        :name=>,
+        :age=>,
+        :apartment_id=>},
+      end
+
+    end
+  end
+end
