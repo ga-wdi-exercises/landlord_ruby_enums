b77522a05eec7f08c6925693c155578a1f687de0
Vince Campanale <vcampanale0294@gmail.com>
2017-02-08 16:58:12 -0500

    completed steps 3c and 3d

--- app.rb
+++ app.rb
@@ -18,76 +18,86 @@ end
   # Explain how the data is structured
   # What are the properties for each of the two types of hashes
 
+  #Data is separated into two categories: apartments and tenants
+  # - Each apartment has 4 fields:
+  #       ID (number), address (string), monthly_rent (number), square_feet (number)
+  # - Each tenant has 4 fields:
+  #       ID (number), name (string), age (number), apartment_id (number)
+
 # Use enumerables to -
 # puts "Print all the addresses for the apartments".upcase
-# apartments.each do |apartment|
-#   puts apartment[:address]
+# ruby_apartments.each do |ruby_apartment|
+#   puts ruby_apartment.address
 # end
 #
 # puts '# ' * 20
 # puts ' '
 # puts "Print all the names for tenants".upcase
-# tenants.each do |tenant|
-#   puts tenant[:name]
+# ruby_tenants.each do |ruby_tenant|
+#   puts ruby_tenant.name
 # end
 #
 # puts '# ' * 20
 # puts ' '
 # puts "Print only apartments that are less then 700 in rent".upcase
-# rent_less_than_700 = apartments.select do |apartment|
-#   apartment[:monthly_rent] < 700
+# rent_less_than_700 = ruby_apartments.select do |ruby_apartment|
+#   ruby_apartment.monthly_rent < 700
 # end
-# puts rent_less_than_700
+# puts rent_less_than_700.inspect
 #
 # puts '# ' * 20
 # puts ' '
 # puts "Print only tenants that are over the age of 44".upcase
-# tenants_over_44 = tenants.select do |tenant|
-#   tenant[:age] > 44
+# tenants_over_44 = ruby_tenants.select do |ruby_tenant|
+#   ruby_tenant.age > 44
 # end
-# puts tenants_over_44
+# puts tenants_over_44.inspect
 #
 # puts '# ' * 20
 # puts ' '
 # puts "Print only tenants that have an apartment id of 1".upcase
-# tenants_in_apt_1 = tenants.find_all { |tenant| tenant[:apartment_id] == 1 }
-# puts tenants_in_apt_1
-#
+# tenants_in_apt_1 = ruby_tenants.find_all { |ruby_tenant| ruby_tenant.apartment_id == 1 }
+# puts tenants_in_apt_1.inspect
+
 # puts '# ' * 20
 # puts ' '
 # puts "Print all the tenants in order from youngest to oldest".upcase
-# tenants_descending_by_age = tenants.sort_by { |tenant| tenant[:age] * -1 }
-# puts tenants_descending_by_age
+# tenants_descending_by_age = ruby_tenants.sort_by { |ruby_tenant| ruby_tenant.age * -1 }
+# puts tenants_descending_by_age.inspect
 #
 # puts '# ' * 20
 # puts ' '
 # puts "Print the names of all the tenants alphabetically".upcase
-# tenants_alphabetical = tenants.sort_by { |tenant| tenant[:name] }
-# puts tenants_alphabetical
+# tenants_alphabetical = ruby_tenants.sort_by { |ruby_tenant| ruby_tenant.name }
+# puts tenants_alphabetical.inspect
 #
 # ## More challenging
 #
 # puts '# ' * 20
 # puts ' '
 # puts "When printing tenants also print out the address that the tenant resides in.".upcase
-# tenants.each do |tenant|
-#   name = tenant[:name]
-#   tenant_apt_id = tenant[:apartment_id]
-#   apt = apartments.select { |apartment| apartment[:id] == tenant_apt_id }
-#   address = apt[0][:address] #note: apt is an array, so you must select the element you want to access the hash attributes
+# ruby_tenants.each do |ruby_tenant|
+#   name = ruby_tenant.name
+#   tenant_apt_id = ruby_tenant.apartment_id
+#   apt = ruby_apartments.select { |ruby_apartment| ruby_apartment.id == tenant_apt_id }
+#   address = apt[0].address #note: apt is an array, so you must select the element you want to access the hash attributes
 #   puts "#{name.to_s} lives at #{address}."
 # end
 #
 # puts '# ' * 20
 # puts ' '
 # puts "When printing all apartments, under each apartment print all of its tenants.".upcase
-# apartments.each do |apartment|
-#   apt_id = apartment[:id]
-#   tenants_in_apt = tenants.select { |tenant| tenant[:apartment_id] == apt_id }
-#   puts "Apartment #{apt_id.to_s}"
-#   puts tenants_in_apt
+# ruby_apartments.each do |ruby_apartment|
+#   apt_id = ruby_apartment.id
+#   tenants_in_apt = ruby_tenants.select { |ruby_tenant| ruby_tenant.apartment_id == apt_id }
+#   puts "Apartment #{apt_id.to_s}:"
+#   tenants_in_apt.each do |tenant|
+#     puts tenant.name
+#   end
 #   puts ' '
 # end
+
+
 input = ""
 
 until input == 'exit'
@@ -110,30 +120,30 @@ until input == 'exit'
     puts ' '
     #list all apartments in terminal
     puts "Printing all the addresses for the apartments..."
-    apartments.each do |apartment|
-      puts apartment[:address]
+    ruby_apartments.each do |ruby_apartment|
+      puts ruby_apartment.address
     end
   elsif input == "2"
     puts ' '
     #list all tenants in terminal
     puts "Printing all the names for tenants..."
-    tenants.each do |tenant|
-      puts tenant[:name]
+    ruby_tenants.each do |ruby_tenant|
+      puts ruby_tenant.name
     end
   elsif input == "3"
     #list all apartments and the tenants in them
     puts ' '
     puts "Printing all apartments with list of tenants in each one..."
-    apartments.each do |apartment|
-      apt_id = apartment[:id]
-      tenants_in_apt = tenants.select { |tenant| tenant[:apartment_id] == apt_id }
+    ruby_apartments.each do |apartment|
+      apt_id = apartment.id
+      tenants_in_apt = ruby_tenants.select { |tenant| tenant.apartment_id == apt_id }
       puts ' '
       puts ' '
       puts ' '
-      puts "Apartment #{apt_id.to_s}, Address: #{apartment[:address].to_s}"
+      puts "Apartment #{apt_id.to_s}, Address: #{apartment.address.to_s}"
       puts ' '
       tenant_names = tenants_in_apt.each do |tenant|
-        puts tenant[:name]
+        puts tenant.name
       end
     end
   elsif input == "4"
@@ -144,10 +154,10 @@ until input == 'exit'
     tenant_to_find = gets.chomp
     puts ' '
     puts "Finding #{tenant_to_find}..."
-    tenant_found = tenants.select { |tenant| tenant[:name] == tenant_to_find }
-    tenant_apartment_id = tenant_found[0][:apartment_id]
-    tenant_apartment = apartments.select { |apartment| apartment[:id] == tenant_apartment_id }
-    puts "#{tenant_to_find} lives at #{tenant_apartment[0][:address].to_s}, apartment id: #{tenant_apartment_id}"
+    tenant_found = ruby_tenants.select { |tenant| tenant.name == tenant_to_find }
+    tenant_apartment_id = tenant_found[0].apartment_id
+    tenant_apartment = ruby_apartments.select { |apartment| apartment.id == tenant_apartment_id }
+    puts "#{tenant_to_find} lives at #{tenant_apartment[0].address.to_s}, Apartment ID: #{tenant_apartment_id}"
   elsif input == "5"
 
   elsif input == "6"
