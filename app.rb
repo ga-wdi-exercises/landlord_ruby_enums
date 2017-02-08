55638649b7b0e6ae0b51308c14a2806158d6e639
jared edwards <jredwards519@gmail.com>
2017-02-08 14:02:32 -0500

    enumerables 1 & 2

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require "pry"
 require_relative "data"
 apartments = data[:apartments]
 tenants = data[:tenants]
@@ -7,8 +8,8 @@ tenants = data[:tenants]
   # What are the properties for each of the two types of hashes
 
 # Use enumerables to -
-  # Print all the addresses for the apartments
-  # Print all the names for tenants
+  # Print all the addresses for the apartments DONE
+  # Print all the names for tenants DONE
   # Print only apartments that are less then 700 in rent
   # Print only tenants that are over the age of 44
   # Print only tenants that have an apartment id of 1
@@ -18,3 +19,27 @@ tenants = data[:tenants]
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
   # When printing all apartments, under each apartment print all of its tenants
+
+addresses = []
+dwellers = []
+lessThan = []
+
+apartments.each{|apartment| puts apartment[:address]}
+apartments.each{|apartment| addresses << apartment[:address]} # array
+
+tenants.each{|tenant| puts tenant[:name]}
+tenants.each{|tenant| dwellers << tenant[:name]} # array
+
+
+apartments.each{|apartment| puts apartment[:monthly_rent] < 700}
+apartments.find_all{|rent| lessThan << rent[:monthly_rent] < 700} # array
+
+
+
+
+
+# apartments[:address].find_all {|address| }
+
+
+
+binding.pry
