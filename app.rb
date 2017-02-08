984f628d70232512c270e8c328994447edbb8d8b
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-02-08 18:51:17 -0500

    finished Part 1

--- app.rb
+++ app.rb
@@ -2,19 +2,49 @@ require_relative "data"
 apartments = data[:apartments]
 tenants = data[:tenants]
 
-# First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
-  # Explain how the data is structured
-  # What are the properties for each of the two types of hashes
+## Explain how the data is structured:
+#The data is in a function with two arrays (apartments and tenants), within the arrays there are hashes with details for each individual apartment or tenant.
+## What are the properties for each of the two types of hashes
+# tenants: id, name, age, and apartment
+# apartments: id, address, monthly_rent, square_feet
 
-# Use enumerables to -
-  # Print all the addresses for the apartments
-  # Print all the names for tenants
-  # Print only apartments that are less then 700 in rent
-  # Print only tenants that are over the age of 44
-  # Print only tenants that have an apartment id of 1
-  # Print all the tenants in order from youngest to oldest
-  # Print the names of all the tenants alphabetically
-
-  ## More challenging
-  # When printing tenants also print out the address that the tenant resides in.
-  # When printing all apartments, under each apartment print all of its tenants
+# return {apartments:apartments, tenants:tenants}
+# apartments.each do |apt|  # Prints all the addresses for the apartments
+#   print apt[:address]
+# end
+# tenants.each do |tenant|
+#   apartments.each do |apt|# Prints all the names for tenants # When printing tenants also print out the address that the tenant resides in.
+#     if tenant[:apartment_id] == apt[:id]
+#     puts "#{tenant[:name]} #{apt[:address]}"
+#     end
+#   end
+# end
+# # apartments.each do |apt|
+#   if apt[:monthly_rent] < 700  # Print only apartments that are less then 700 in rent
+#     puts apt[:address]
+#   end
+# end
+# tenants.each do |tenant|
+#   if tenant[:age] > 44 # Print only tenants that are over the age of 44
+#     puts tenant[:name]
+#   end
+# end
+# tenants.each do |tenant|
+#   if tenant[:apartment_id] == 1  # Print only tenants that have an apartment id of 1
+#     puts tenant[:name]
+#   end
+# end
+#  puts tenants.sort_by{|tenant| tenant[:age]} #  Print all the tenants in order from youngest to oldest
+#
+#  puts tenants.sort_by{|name| name[:name]} # Print the names of all the tenants alphabetically
+# apartment_tenants = []
+#  tenants.each do |tenant|
+#    # When printing all apartments, under each apartment print all of its tenants
+#    apartment_tenants << tenant[:name]
+#   apartments.each do |apt|
+#     if apt[:id] == tenant[:apartment_id]
+#       puts apt
+#       puts apartment_tenants
+#     end
+#   end
+# end
--- data.rb
+++ data.rb
@@ -74,50 +74,7 @@ def data
     {:id=>49, :name=>"Rowland Wiegand", :age=>90, :apartment_id=>11},
     {:id=>50, :name=>"Rigoberto Armstrong IV", :age=>60, :apartment_id=>1}
   ]
-  # return {apartments:apartments, tenants:tenants}
-  # apartments.each do |apt|  # Prints all the addresses for the apartments
-  #   print apt[:address]
-  # end
-  tenants.each do |tenant|
-    apartments.each do |apt|# Prints all the names for tenants # When printing tenants also print out the address that the tenant resides in.
-      if tenant[:apartment_id] == apt[:id]
-      puts "#{tenant[:name]} #{apt[:address]}"
-      end
-    end
-  end
-  # apartments.each do |apt|
-  #   if apt[:monthly_rent] < 700  # Print only apartments that are less then 700 in rent
-  #     puts apt[:address]
-  #   end
-  # end
-  # tenants.each do |tenant|
-  #   if tenant[:age] > 44 # Print only tenants that are over the age of 44
-  #     puts tenant[:name]
-  #   end
-  # end
-  # tenants.each do |tenant|
-  #   if tenant[:apartment_id] == 1  # Print only tenants that have an apartment id of 1
-  #     puts tenant[:name]
-  #   end
-  # end
-  #  puts tenants.sort_by{|tenant| tenant[:age]} #  Print all the tenants in order from youngest to oldest
-  #
-  #  puts tenants.sort_by{|name| name[:name]} # Print the names of all the tenants alphabetically
-
-  apartments.each do |apt|   # When printing all apartments, under each apartment print all of its tenants
-    tenants.each do |tenant|
-      if apt[:id] == tenant[:apartment_id]
-        
-      end
-    end
-  end
+  return {apartments:apartments, tenants:tenants}
 
 
 end
-## Explain how the data is structured:
-#The data is in a function with two arrays (apartments and tenants), within the arrays there are hashes with details for each individual apartment or tenant.
-## What are the properties for each of the two types of hashes
-# tenants: id, name, age, and apartment
-# apartments: id, address, monthly_rent, square_feet
-
-data()
