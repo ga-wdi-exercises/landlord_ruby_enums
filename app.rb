4a5df3c1de40948b9a780175886278cdbae17941
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-02-08 18:51:04 -0500

    done all before more challenging questions

--- app.rb
+++ app.rb
@@ -19,14 +19,25 @@ tenants = data[:tenants]
   # end
   # puts less_than_700
   # Print only tenants that are over the age of 44
-  over_44 = tenants.select do |tenant|
-    tenant[:age] > 44
-  end
-  puts over_44
+  # over_44 = tenants.select do |tenant|
+  #   tenant[:age] > 44
+  # end
+  # puts over_44
   # Print only tenants that have an apartment id of 1
+  # a_id_1 = tenants.select do |tenant|
+  #   tenant[:apartment_id] == 1
+  # end
+  # print a_id_1
   # Print all the tenants in order from youngest to oldest
+  # y_to_o = tenants.sort_by do |tenant|
+  #   tenant[:age]
+  # end
+  # puts y_to_o
   # Print the names of all the tenants alphabetically
-
+  # alpha = tenants.sort_by do |tenant|
+  #   tenant[:name]
+  # end
+  # puts alpha
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
   # When printing all apartments, under each apartment print all of its tenants
