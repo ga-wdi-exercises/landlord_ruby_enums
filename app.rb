27315ad45e4c0c35240135974ea68af37fe15f55
Mike Nabil <maikel_nabil@yahoo.com>
2017-02-08 17:54:11 -0500

    initial commit

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative "data"
 apartments = data[:apartments]
 tenants = data[:tenants]
 
-# First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
+# First, Open the data.rb and inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
   # What are the properties for each of the two types of hashes
 
