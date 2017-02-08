b728ce77c49b794af1f8cb450eb18b404cd4882f
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-02-08 18:33:47 -0500

    answered first 5 questions

--- app.rb
+++ app.rb
@@ -5,12 +5,24 @@ tenants = data[:tenants]
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
   # What are the properties for each of the two types of hashes
+  # apartments: id, address, monthly rent, and square feet.
+  # tenants: id, name, age, apartment id.
 
 # Use enumerables to -
   # Print all the addresses for the apartments
+  # apartments.each{|apartment| puts apartment[:address]}
   # Print all the names for tenants
+  # tenants.each{|full_name| puts full_name[:name]}
   # Print only apartments that are less then 700 in rent
+  # less_than_700 = apartments.select do |apartment|
+  #   apartment[:monthly_rent] < 700
+  # end
+  # puts less_than_700
   # Print only tenants that are over the age of 44
+  over_44 = tenants.select do |tenant|
+    tenant[:age] > 44
+  end
+  puts over_44
   # Print only tenants that have an apartment id of 1
   # Print all the tenants in order from youngest to oldest
   # Print the names of all the tenants alphabetically
