28618182aed76753ad942fc85cd872545449299d
Mike Nabil <maikel_nabil@yahoo.com>
2017-02-08 20:09:00 -0500

    part 2, mvp added

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require_relative "data"
 apartments = data[:apartments]
 tenants = data[:tenants]
 
+#Part 1
 # First, Open the data.rb and inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
   # What are the properties for each of the two types of hashes
@@ -37,3 +38,14 @@ tenants = data[:tenants]
   # When printing tenants also print out the address that the tenant resides in.
 
   # When printing all apartments, under each apartment print all of its tenants
+
+#Part 2
+puts "Welcome, landlord! For apartments type '1', and for tenants type '2'"
+user_input = gets.chomp.to_i
+if user_input == 1
+  p apartments
+elsif user_input == 2
+  p tenants
+else
+  puts "Sorry, I didn't undersatnd this!"
+end
