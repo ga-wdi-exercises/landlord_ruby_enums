0cbe64a32e3f928311f616684bd846c2ae1c2ede
Andy Flickinger <atflick@gmail.com>
2017-02-08 21:12:35 -0500

    part 1, some of 2 done

--- app.rb
+++ app.rb
@@ -4,17 +4,52 @@ tenants = data[:tenants]
 
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
+      # apartments - array of hashes, each hash is an apartment with an ID
+      # tenants - array of hashes, each hash is a tenant that corresponds to an apartment ID
   # What are the properties for each of the two types of hashes
+      # apartments - id #, :address str, :monthly_rent #, :square_feet #
+      # tenants - :id #, :name str, :age #, :apartment_id #
 
 # Use enumerables to -
   # Print all the addresses for the apartments
-  # Print all the names for tenants
-  # Print only apartments that are less then 700 in rent
-  # Print only tenants that are over the age of 44
+  # apartments.each {|apartment| p apartment[:address]}
+  # # Print all the names for tenants
+  # tenants.each {|tenant| p tenant[:name]}
+  # # Print only apartments that are less then 700 in rent
+  # cheap_apartments = apartments.select {|apartment| apartment[:monthly_rent] < 700}
+  # p cheap_apartments
+  # # Print only tenants that are over the age of 44
+  # old_tenants = tenants.select {|tenant| tenant[:age] > 44}
+  # p old_tenants
   # Print only tenants that have an apartment id of 1
+  # tenant_1 = tenants.select{|tenant| p tenant[:apartment_id] == 1}
+  # p tenant_1
   # Print all the tenants in order from youngest to oldest
-  # Print the names of all the tenants alphabetically
+  # tenant_by_age = tenants.sort_by {|tenant| tenant[:age]}
+  # p tenant_by_age
+  # # Print the names of all the tenants alphabetically
+  # tenants_by_name = tenants.sort_by {|tenant| tenant[:name]}
+  # tenants_by_name.each {|tenant| p tenant[:name]}
 
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
   # When printing all apartments, under each apartment print all of its tenants
+
+# START APP #
+br = "*" *100 +"\n"
+all_apartments = apartments.each {|apartment| apartment}
+all_tenants = tenants.each {|tenant| tenant}
+
+while true
+  puts "#{br} Welcome to Landlord.app, you have the following options: "
+  puts "  1. View all apartments\n  2. View all tenants\n  3. View tenants by apartments\n  4. Find tenant by name\n  5. Make changes \n (Select an option by entering the corresponding number)\n#{br}"
+
+  user_input = gets.chomp
+    if user_input == "1"
+      puts all_apartments
+    elsif user_input == "2"
+      puts all_tenants
+    elsif "3"
+      
+    end
+  end
--- data.rb
+++ data.rb
@@ -21,7 +21,6 @@ def data
     {:id=>19, :address=>"72108 Leuschke Greens", :monthly_rent=>661, :square_feet=>967},
     {:id=>20, :address=>"4705 Rosenbaum Ville", :monthly_rent=>993, :square_feet=>1168}
   ]
-
   tenants = [
     {:id=>1, :name=>"Ms. Maryse Farrell", :age=>87, :apartment_id=>8},
     {:id=>2, :name=>"Maci Hickle II", :age=>28, :apartment_id=>1},
