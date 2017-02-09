271ce573ec1bd81f58ef50d61ee501c82e59493c
Bedig Galladian <bgalladian@gmail.com>
2017-02-08 21:26:15 -0500

    part 1

--- data.rb
+++ data.rb
@@ -76,3 +76,23 @@ def data
   ]
   return {apartments:apartments, tenants:tenants}
 end
+
+#The data is structured in two arrays.  One for apartments and one for tenants.  The apartments have values of ID, address, monthly rent and square feet.  The tenants have ID, name, age, and apartment ID.
+# Print all the addresses for the apartments
+apartments.each{|unit| puts unit[:address]}
+# Print all the names for tenants
+tenants.each{|name| puts name[:name]}
+# Print only apartments that are less then 700 in rent
+low = apartments.each{|unit| unit[:monthly_rent] < 700}
+p low
+# Print only tenants that are over the age of 44
+old_tenants = tenants.each{|age| age[:age] > 44}
+p old_tenants
+# Print only tenants that have an apartment id of 1
+apt_number_1 = apartments.each{|unit| unit[:id] == 1}
+# Print all the tenants in order from youngest to oldest
+young_to_old = tenants.sort_by{|yto| yto[:age]}
+p young_to_old
+# Print the names of all the tenants alphabetically
+alpha = tenants.sort_by{|alpha| alpha[:name]}
+p alpha.each
