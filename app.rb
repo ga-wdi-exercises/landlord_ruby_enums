04791d03426c0c642402c6a9ffb71800f15dcc05
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-08 23:22:41 -0500

    uploading part 1

--- app.rb
+++ app.rb
@@ -18,15 +18,18 @@ tenants = data[:tenants]
   end
 
   # Print only apartments that are less then 700 in rent
-  apartment.each do |tenant|
-    puts tenant[:name]
-  end
-
+  arr = apartments.select {|apartment| apartment[:monthly_rent]< 700}
+  puts arr
   # Print only tenants that are over the age of 44
+  arr = tenants.select {|tenant| tenant[:age]> 44}
+  puts arr
   # Print only tenants that have an apartment id of 1
+  arr = tenants.select {|tenant| tenant[:id] == 1}
+  puts arr
   # Print all the tenants in order from youngest to oldest
+  puts tenants.sort_by {|tenant| tenant[:age]}
   # Print the names of all the tenants alphabetically
-
+  puts tenants.sort_by {|tenant| tenant[:name]}
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
   # When printing all apartments, under each apartment print all of its tenants
