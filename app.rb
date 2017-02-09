2c711c27cef6c79a41479dce9a7497686689fefe
Eva Lai <evalai.dc@gmail.com>
2017-02-08 23:20:39 -0500

    changed all part1 work to pull from ruby_tenant or ruby_apartments

--- app.rb
+++ app.rb
@@ -9,12 +9,10 @@ tenants = data[:tenants]
 ruby_apartments = apartments.each do |apartment|
   Apartment.new(apartment[:id], apartment[:address],apartment[:monthly_rent], apartment[:square_feet])
 end
-puts ruby_apartments
 
 ruby_tenants = tenants.each do |tenant|
   Tenant.new(tenant[:id], tenant[:name], tenant[:age], tenant[:apartment_id])
 end
-puts ruby_tenants
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
   ## The data is structured in apartments to a specific address, monthly_rent, and square_feet so in all the specifics of the actual apartment and the tenants describe their name, age, and apartment_id which are more specific to them.
@@ -25,36 +23,36 @@ puts ruby_tenants
 
 # Use enumerables to -
   # Print all the addresses for the apartments
-    # apartments.each {|apartment| puts apartment[:address]}
+    # ruby_apartments.each {|apartment| puts apartment[:address]}
   # Print all the names for tenants
-    # tenants.each {|tenant| puts tenant[:name]}
+    # ruby_tenants.each {|tenant| puts tenant[:name]}
   # Print only apartments that are less then 700 in rent
-    # less_than_700 = apartments.select do |apartment|
+    # less_than_700 = ruby_apartments.select do |apartment|
       # apartment[:monthly_rent] < 700
     # end
     # puts less_than_700
   # Print only tenants that have an apartment id of 1
-    # apt_id_1 = tenants.select do |tenant|
+    # apt_id_1 = ruby_tenants.select do |tenant|
       # tenant[:apartment_id] ==1
     # end
     # puts apt_id_1
   # Print all the tenants in order from youngest to oldest
-    # puts young_to_old = tenants.sort_by{|tenant| tenant[:age] }
+    # puts young_to_old = ruby_tenants.sort_by{|tenant| tenant[:age] }
   # Print the names of all the tenants alphabetically
-    # puts alphabetical_name = tenants.sort_by{|tenant| tenant[:name]}
+    # puts alphabetical_name = ruby_tenants.sort_by{|tenant| tenant[:name]}
 
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
-    # tenants.each do |tenant|
-    #   apartments.each do |apartment|
+    # ruby_tenants.each do |tenant|
+    #   ruby_apartments.each do |apartment|
     #     if tenant[:apartment_id] == apartment[:id]
     #      puts "#{tenant[:name]} #{apartment[:address]}"
     #     end
     #   end
     # end
   # When printing all apartments, under each apartment print all of its tenants
-    # apartments.each do |apartment|
-    #     house_tenants = tenants.select{|tenant| tenant[:apartment_id] == apartment[:id]}
+    # ruby_apartments.each do |apartment|
+    #     house_tenants = ruby_tenants.select{|tenant| tenant[:apartment_id] == apartment[:id]}
     #   puts apartment
     #   puts house_tenants
     # end
