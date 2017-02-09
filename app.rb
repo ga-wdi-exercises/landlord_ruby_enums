a269e6bd3a641dd473c011f965d151ef33b47137
Ebenezer Williams <ebenezerw@me.com>
2017-02-09 07:32:33 -0500

    part 1

--- app.rb
+++ app.rb
@@ -8,10 +8,33 @@ tenants = data[:tenants]
 
 # Use enumerables to -
   # Print all the addresses for the apartments
+  apartments.each do |apartment|
+    puts apartment[:address]
+  end
+
   # Print all the names for tenants
+  tenants.each do |tenant|
+    puts tenant[:name]
+  end
+
   # Print only apartments that are less then 700 in rent
+  apartments_below_700 = apartments.select do |apartment|
+    apartment[:monthly_rent] < 700
+  end
+  puts apartments_below_700
+
   # Print only tenants that are over the age of 44
+  tenants_over_44 = tenants.select do |tenant|
+    tenant[:age] > 44
+  end
+  puts tenants_over_44
+
   # Print only tenants that have an apartment id of 1
+  id_1 = tenants.select do |id|
+    id[:id] == 1
+  end
+  puts id_1
+
   # Print all the tenants in order from youngest to oldest
   # Print the names of all the tenants alphabetically
 
