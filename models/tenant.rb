c43bfe1d4509ba8772ee0cb691cc3b90e51cb5b7
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-02-09 08:46:56 -0500

    part 3 a

---
+++ models/tenant.rb
@@ -0,0 +1,15 @@
+require "pry"
+
+class Tenant
+  attr_accessor :id, :name, :age, :apartment_id
+  
+  def initialize(id, name, age, apartment_id)
+    @id = id
+    @name = name
+    @age = age
+    @apartment_id = apartment_id
+  end
+end
+
+
+binding.end
