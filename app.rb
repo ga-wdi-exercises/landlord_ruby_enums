d5233a009cc9c9196e7fd9af2b1cf65fcd657402
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-02-09 06:58:04 -0500

    part 2 works

--- app.rb
+++ app.rb
@@ -3,47 +3,119 @@ require 'pry'
 require_relative "data"
 apartments = data[:apartments]
 tenants = data[:tenants]
+playing = true
+until playing ==false
+  puts 'Action? 1. Get particular apartment 2. Get particular tenant 3. Get all tenants 4. Get all apartments'
+  input = gets.chomp
+  input = input.to_i
 
-
-apartments.each do |x|
-  puts x[:address]
-end
-
-tenants.each do |x|
-  puts x[:name]
-end
-
-apartments.each do |x|
-  if x[:rent] < 700
-    puts x
+  while input == 3
+    tenants.each do |x|
+      puts x
+    end
+    input = 0
   end
-end
 
-tenants.each do |x|
-  if x[:age] > 44
-    puts x
+  while input == 4
+    apartments.each do |x|
+      puts x
+    end
+    input = 0
   end
-end
 
-tenants.each do |x|
-  if x[:id] == 1
-    puts x
+  while input == 1
+    puts 'Which ID?'
+    inp = gets.chomp
+    inp = inp.to_i
+    apartments.each do |x|
+      if inp == x[:id]
+        puts x
+      end
+    end
+    input = 0
   end
+
+  while input == 2
+    puts 'Which ID?'
+    inp = gets.chomp
+      inp = inp.to_i
+      tenants.each do |x|
+        if inp == x[:id]
+          puts x
+        end
+      end
+      input = 0
+    end
+
+    puts 'keep going? 1=yes 2=no'
+    cont = gets.chomp
+    cont = cont.to_i
+    if cont == 2
+      playing = false
+    end
+
 end
 
-sort_tenants = tenants.sort {|a,b| a[:age] <=> b[:age] }
-# Use enumerables to -
-  # Print all the addresses for the apartments
-  # Print all the names for tenants
-  # Print only apartments that are less then 700 in rent
-  # Print only tenants that are over the age of 44
-  # Print only tenants that have an apartment id of 1
-  # Print all the tenants in order from youngest to oldest
-  # Print the names of all the tenants alphabetically
 
-  ## More challenging
-  # When printing tenants also print out the address that the tenant resides in.
-  # When printing all apartments, under each apartment print all of its tenants
+
+
+
+# apartments.each do |x|
+#   puts x[:address]
+# end
+#
+# tenants.each do |x|
+#   puts x[:name]
+# end
+#
+# apartments.each do |x|
+#   if x[:monthly_rent] < 700
+#     puts x
+#   end
+# end
+#
+# tenants.each do |x|
+#   if x[:age] > 44
+#     puts x
+#   end
+# end
+#
+# tenants.each do |x|
+#   if x[:id] == 1
+#     puts x
+#   end
+# end
+#
+# sort_tenants = tenants.sort {|a,b| a[:age] <=> b[:age] }
+#
+# new_sort_tenants = tenants.sort {|a,b| a[:name] <=> b[:name]}
+#
+# puts 'break'
+# puts sort_tenants
+#
+# puts 'break'
+# puts new_sort_tenants
+# puts 'break'
+#
+# tenants.each do |x|
+#   puts x[:name]
+#   apartments.each do |y|
+#     if y[:id] == x[:apartment_id]
+#       puts y[:address]
+#     end
+#   end
+# end
+#
+# apartments.each do |x|
+#   puts x[:address]
+#   tenants.each do |y|
+#     if x[:id] == y[:apartment_id]
+#       puts y[:names]
+#     end
+#   end
+# end
+#
+
 binding.pry
 
 puts 'program resumes here.'
