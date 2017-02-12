5c5804f23f3b061c8bae1121e4651c4ee248bc49
xgironx <xgironx@gmail.com>
2017-02-12 12:49:48 -0500

    fixed tenants/apartment console prompt item #3 and #4, to OO Ruby way

--- app.rb
+++ app.rb
@@ -1,105 +1,103 @@
 ##################################
 # 201702081919L  EL MIERCOLES   JAY
+# https://github.com/ga-wdi-exercises/landlord_ruby_no_AR
 ##################################
+#
+# Part 1 - Practice With Enumerables Read the comments in app.rb to follow the directions practicing enumerables Store the enumerable methods into variables and use puts to verify the results of your methods.
+# Part 2 - Build the App
+# Comment out any functional code in app.rb, but save it, you can leverage the code from Part 1 to write out parts of this application.
+# Bob Pizza is old school and he wants a sick command line interface for this app. Below are Bob's user stories. It is essential that he implement the MVP user stories. The Silver and Gold user stories are considered to be bonuses.
+#
+# MVP
+# - I should have a numbered interface for my application so that I can just type in a number to access different parts of my program.
+# - I should be allowed an option to view all the apartments so that I can get an overview of my properties.
+# - I should be allowed an option view all the tenants so that I can get an overview of my tenants
 
+# Silver
+# - I should be allowed an option to view all the apartments with their tenants so that I can get an overview of Occupancy.
+# - I should be able to find where a tenant lives based on tenant name in case of emergencies.
 
+# Gold
+# - I should be allowed to remove a tenant from an apartment so that I can track evictions/lease ends
+# - I should be allowed to add a tenant to an apartment so that I can track occupancy.
+# - I should be allowed to create a new apartment so that I can track new properties in my books
+# - I should be allowed to create a new tenant so that I can track new tenants in my books.
+# ##################################
 
-# First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
-  # Explain how the data is structured
-  DATA SHARES A COMMON KEY,
-  APARTMENTS HAS "id":        {:id=>1, ...
-                    # {:id=>1, :address=>"9841 Tanner Key", :monthly_rent=>606, :square_feet=>779},
-  TENANTS HAS "apartment_id"  ... :apartment_id=>8}
-                # {:id=>1, :name=>"Ms. Maryse Farrell", :age=>87, :apartment_id=>8},
+require_relative "data"
+require_relative "models/apartment"
+require_relative "models/tenant"
+apartments = data[:apartments]
+tenants = data[:tenants]
+apartments_class = []
+tenants_class = []
 
+apartments.each do |apartment|
+  create_apartment = Apartment.new(apartment[:id],apartment[:address],apartment[:monthly_rent],apartment[:square_feet])
+  apartments_class << create_apartment
+end
 
-  # What are the properties for each of the two types of hashes
-    APARTMENTS HAS
-        :id, :address, :monthly_rent, :square_feet
-            # {:id=>1, :address=>"9841 Tanner Key", :monthly_rent=>606, :square_feet=>779},
-    TENANTS HAS
-        :id, :name, :age, :apartment_id
-            # {:id=>1, :name=>"Ms. Maryse Farrell", :age=>87, :apartment_id=>8},
+tenants.each do |tenant|
+  create_tenant = Tenant.new(tenant[:id],tenant[:name],tenant[:age],tenant[:apartment_id])
+  tenants_class << create_tenant
+end
 
+selection = nil
+while selection != 0
+  puts ''
+  puts ''
+  puts "*****************************
+1: Show me all the apartments
+2: Introduce me to all the tenants
+3: Show me which tenants live in each apartment
+4: Show me 'i know where you live' stalker-y address for a given tenant
+0: You still here?  Exit the program"
 
-              # ---------------------------------
-  require_relative "data"
-  apartments = data[:apartments]
-  tenants = data[:tenants]
-  # Use enumerables to -
-              # ENUMERABLES: # each  is an enumerable # users = ["Alice", "Bob", "Carol"] # users.each do |user| #  puts user # end
-              # ALL
+  selection = gets.chomp.to_i
+
+  if selection == 1
               # Print all the addresses for the apartments
-              puts ''
-              apartments.each do |apartment|
+    puts ''
+    apartments.each do |apartment|
               # puts apartment                    # {:id=>19, :address=>"72108 Leuschke Greens", :monthly_rent=>661, :square_feet=>967} # {:id=>20, :address=>"4705 Rosenbaum Ville", :monthly_rent=>993, :square_feet=>1168} #   Wed Feb 08 11:50:12 ~/wdi/homework/due_201702090900L/landlord_ruby_no_AR (master *)
               # puts "apartments_address: #{apartment[:address]}"  # [22] pry(main)*   puts "unit Addr: #{apt[:address]}" [22] pry(main)* end unit Addr: 9841 Tanner Key unit Addr: 6971 Corwin Locks
-              p    "apartment_address: #{apartment[:address]}"
-            end
+      p    "apartment_address:   #{apartment[:address]}"
+    end
+  end
 
               # Print all the names for tenants
-              puts ''
-              tenants.each do |tenant|
-              puts "tenant_name: #{tenant[:name]}}"  # THIS WORKS THANKS TO WILL
+  if selection == 2
+    puts ''
+    tenants.each do |tenant|
+      puts "tenant_name:   #{tenant[:name]}}"  # THIS WORKS THANKS TO WILL
+    end
+  end
 
-
-              # SELECT
-              # Print only apartment that are less then 700 in rent
-              puts ''
-              apartment =  apartments.select do |apartment|
-                apartment[:monthly_rent] < 700
-                  # puts "apartments_cheap: #{apartment[:address]}"
-                  p     "apartment_cheap:  #{apartment[:address]}"
-              end
-
-
-              # SELECT
-              # Print only tenants that are over the age of 44
-              puts ''
-              tenant = tenants.select do |tenant|
-                tenant[:age] > 44
-              # puts "tenants_44: #{tenants44[:name]}" # puts "tenants_44: #{tenants44[:name][:age]}" # puts "tenants_44: #{tenants44[:name]} , #{tenants44[:age]}" # puts "tenants_44: name: #{tenants44[:name]} , age: #{tenants44[:age]}"
-              p    "tenant_44: name: #{tenant[:name]} , age: #{tenant[:age]}"
-            end
-
-              # SORT
-              # WILLS SORT BY
-              # Print the names of all the tenants alphabetically
-              # names.sort_by {|h| h.keys[0]} # array_o f_hashes.sort_by { |hash| hash[:key_to_sort] } # search_results.sort_by { |user| user.age.to_i } # tenants.each do sort # sort { |a, b| block.bind(b).call <=> block.bind(a).call } # tenant = tenants.sort do |tenant| # sorted = dataarray.sort {|a,b| a[:zip] <=> b[:zip]} # people.sort_by { |k, v| v[:age] } # {:id=>1, :name=>"Ms. Maryse Farrell", :age=>87, :apartment_id=>8}, # tenants_alpha = tenants.sort {|id, name| v[:name]} do # tenants_alpha = tenants.sort {|id, name| v[name]} do |tenants_alpha| # tenants_alpha = tenants.sort_by {|tenants_alpha| tenants_alpha.name} do |tenants_alpha| # puts    tenants.sort_by {|tenant| tenant[:name]}  # THIS WORKS THANKS TO WILL # tenant_alpha = tenants.sort_by {|tenant| tenant[:name]} # p "tenant_alpha, by name: " + tenant_alpha # p "tenant_alpha, by name: #{tenant[:name]}"
-              puts ''
-              puts "WILLS SORT BY"
-              puts "sort by name #{tenants.sort_by {|tenant| tenant[:name]}}"  # THIS WORKS THANKS TO WILL
-
-
-              # SELECT
-              # Print only tenants that have an apartment id of 1
-              puts ''
-              apartment_id_is_1 =  apartments.select do |apartment_id_is_1|
-              apartment_id_is_1[:id] = 1
-              p     "apartment_id_is_1:  #{apartment_id_is_1[:address]}"
-            end
-
-              # SORT
-              # Print all the tenants in order from youngest to oldest
-              puts ''
-              puts "sort by age desc  #{tenants.sort_by {|tenant| tenant[:age]}.reverse}"  # THIS WORKS THANKS TO WILL
-
-
-              ## More challenging
-              # THIS ONE IS ONE STEP BEYOND FOR ME
-              # RIGHT NOW AS IT REQ A JOIN BETWEEN THE ARRAYS ON
-              # THE APT ID FIELD
-              # SELECT
-              # When printing tenants also print out the address that the tenants resides in.
-              # puts ''
-              # THIS ONE WORKS ONLY WHEN EVERYTHING ABOVE IS
-              # COMMENTED
-              tenants.each do |tenant|
-                 apartments.each do |apt|# Prints all the names for tenants # When printing tenants also print out the address that the tenant resides in.
-                   if tenant[:apartment_id] == apt[:id]
-                   puts "#{tenant[:name]} #{apt[:address]}"
-                   end
-                 end
-               end
-
+              # 201702121055L   EL DOMINGO   JAY
+              # PRINT ALL THE NAMES FOR TENANTS IN A GIVEN APARTMENT
               # When printing all apartments, under each apartment print all of its tenants
+  if selection == 3
+      apartments_class.each do |apartment|
+        puts "Apartment: " + apartment.id.to_s
+        all_tenants = tenants_class.select do |tenant|
+          if (tenant.apartment_id == apartment.id)
+            puts "   Tenant who lives in this apartment: " + tenant.name
+          end
+        end
+      end
+  end
+
+  if selection == 4
+    puts "What\'s yer tenant\'s name"
+    puts ''
+    puts ''
+    tenant_name = gets.chomp
+    find_tenant = tenants_class.find do |tenant|
+      tenant_name == tenant.name                # VAR FOR THE INPUT TENANTS NAME
+    end
+    find_address = apartments_class.find do |apartment|
+      find_tenant.apartment_id == apartment.id  # SHOW THE ADDR FOR A GIVEN TENANT NAME
+    end
+    puts find_address.address
+  end
+end                    # END WHILE LOOP
