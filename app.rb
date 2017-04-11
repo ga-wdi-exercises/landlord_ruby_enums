dc9fe3689a6e96732c54f5cba47487994032df43
Marcus Linn Miller Jr <marcusmiller220@gmail.com>
2017-04-11 19:51:45 -0400

    Final commit

--- app.rb
+++ app.rb
@@ -6,49 +6,7 @@ tenants = data[:tenants]
 ruby_apartments = []
 ruby_tenants = []
 
-
-# First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
-  # Explain how the data is structured
-  # What are the properties for each of the two types of hashes
-
-# Use enumerables to -
-  # Print all the addresses for the apartments
-  # Print all the names for tenants
-  # Print only apartments that are less then 700 in rent
-  # Print only tenants that are over the age of 44
-  # Print only tenants that have an apartment id of 1
-  # Print all the tenants in order from youngest to oldest
-  # Print the names of all the tenants alphabetically
-
-  ## More challenging
-  # When printing tenants also print out the address that the tenant resides in.
-  # When printing all apartments, under each apartment print all of its tenants
-
-  # The data is structured in a method with a return of two key value pairs apartments and tenants
-  # For apartments the properties are (id, address, monthly_rent, square_feet) For tenants (id, name, age, apartment id)
-  # apartments.each{|apartment| puts apartment[:address]}
-  # tenants.each{|apartment| puts tenant[:name]}
-  # apartments.select{|apartment| apartment[:monthly_rent] < 700}
-  # tenants.select{|tenant| tenant[:age] < 44}
-  # tenants.select{|tenant| tenant[:id] == 1}
-  # tenants.sort_by{|tenant| tenant[:age]}
-  # tenants.sort_by{|tenant| tenant[:name]}
-
-  # tenants.each do |tenant|
-  # puts tenant.name
-  # whose_apartment = apartments.find{|apartment| apartment[:id] == tenant[:apartment_id]}
-  # puts whose_apartment.name
-  # end
-
-  # apartments.each do |apartment|
-  # puts apartment.address
-  # tenants_per_apartment = tenants.select{|tenant| tenant.apartment_id == apartment.id}
-  # tenants_per_apartment.each do |tenant|
-  # puts tenant.name
-  # end
-  # end
-
-require 'pry'
+# require 'pry'
 
   def program_select(apts, tnts, data)
       if data == 1
@@ -81,6 +39,46 @@ apartments.each do |apartment|
     ruby_apartments.push Apartment.new(apartment[:id], apartment[:address], apartment[:monthly_rent], apartment[:square_feet])
 end
 
-binding.pry
+# binding.pry
+#
+# puts "End of line"
+# First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
+  # Explain how the data is structured
+  # What are the properties for each of the two types of hashes
 
-puts "End of line"
+# Use enumerables to -
+  # Print all the addresses for the apartments
+  # Print all the names for tenants
+  # Print only apartments that are less then 700 in rent
+  # Print only tenants that are over the age of 44
+  # Print only tenants that have an apartment id of 1
+  # Print all the tenants in order from youngest to oldest
+  # Print the names of all the tenants alphabetically
+
+  ## More challenging
+  # When printing tenants also print out the address that the tenant resides in.
+  # When printing all apartments, under each apartment print all of its tenants
+
+  # The data is structured in a method with a return of two key value pairs apartments and tenants
+  # For apartments the properties are (id, address, monthly_rent, square_feet) For tenants (id, name, age, apartment id)
+  # ruby_apartments.each{|apartment| puts apartment.address}
+  # ruby_tenants.each{|apartment| puts tenant.name}
+  # ruby_apartments.select{|apartment| apartment.monthly_rent < 700}
+  # ruby_tenants.select{|tenant| tenant.age < 44}
+  # ruby_tenants.select{|tenant| tenant.id == 1}
+  # ruby_tenants.sort_by{|tenant| tenant.age}
+  # ruby_tenants.sort_by{|tenant| tenant.name}
+
+  # ruby_tenants.each do |tenant|
+  #     puts tenant.name
+  #     whose_apartment = ruby_apartments.find{|apartment| apartment.id == tenant.apartment_id}
+  #     puts whose_apartment.name
+  # end
+
+  # ruby_apartments.each do |apartment|
+  # puts apartment.address
+  # ruby_tenants_per_apartment = tenants.select{|tenant| tenant.apartment_id == apartment.id}
+  # ruby_tenants_per_apartment.each do |tenant|
+  # puts tenant.name
+  # end
+  # end
