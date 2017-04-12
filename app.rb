84d63f83fe96698be50dbbeee4fa1bd34befca9d
Jeong Hoon Koo <junghoone17@gmail.com>
2017-04-12 10:04:11 -0400

    landLord

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
-# require_relative "data"
-# apartments = data[:apartments]
-# tenants = data[:tenants]
-#
+require_relative "data"
+require_relative "models/apartment"
+# require_relative "models/tenant"
+apartments = data[:apartments]
+tenants = data[:tenants]
+
 # # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
 #   # Explain how the data is structured
 #     # The data is structured in a method which returns two keys: apartments & tenants
@@ -46,16 +48,26 @@
 #   }
 #   # When printing all apartments, under each apartment print all of its tenants
 
-value = 0
-while value >= 0 do
+# value = 0
+# while value >= 0 do
+#
+#     puts "show me a number -1 to quit"
+#
+#     value = gets.chomp.to_i
+#
+#     if value == 0
+#       apartments.each{|apartment| puts apartment[:address]}
+#     elsif value == 1
+#       tenants.each{|tenant| puts tenant[:name]}
+#     end
+# end
 
-    puts "show me a number -1 to quit"
 
-    value = gets.chomp.to_i
+ruby_apartments = []
 
-    if value == 0
-      apartments.each{|apartment| puts apartment[:address]}
-    elsif value == 1
-      tenants.each{|tenant| puts tenant[:name]}
-    end
-end
+apartments.each {|apartment|
+  ruby_apartments <<
+  Apartment.new(apartment[:id], apartment[:address],
+  apartment[:monthly_rent], apartment[:square_feet])
+}
+puts ruby_apartments
