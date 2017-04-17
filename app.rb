78480771b7993c80c0d557d43a176687f0f833e8
Luisa Gonzalez <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-04-17 16:11:28 -0400

    landlord ruby AR Changes

--- app.rb
+++ app.rb
@@ -7,60 +7,60 @@ apartments = data[:apartments]
 tenants = data[:tenants]
 
 
-# The data is structured into 2 arrays classed as apartments and tenants
-# The apartment array contains properties including id,address, monthly_rent,and square_feet
-# The tenant array is contains properties including id, name, age, apartment_id
+The data is structured into 2 arrays classed as apartments and tenants
+The apartment array contains properties including id,address, monthly_rent,and square_feet
+The tenant array is contains properties including id, name, age, apartment_id
 # First, Open the data.rb an inspect the data. Identify and write, in comments, the following:
   # Explain how the data is structured
   # What are the properties for each of the two types of hashes
 
 # Use enumerables to - # Print all the addresses for the apartments
-# apartments = ["address"]
-# apartments.each do |user|
-# puts user
-# end
+apartments = ["address"]
+apartments.each do |user|
+puts user
+end
 
 
 # Print all the names for tenants
-# tenants = ["name"]
-# tenants.each do |name|
-# puts tenant
-# end
+tenants = ["name"]
+tenants.each do |name|
+puts tenant
+end
 
 
 
 # Print only apartments that are less then 700 in rent
-# apartment_address = ["9841 Tanner Key","630 McDermott Islands","2100 Mitchel Valleys","37899 Kutch Corners","29724 Ryan Ranch","80498 Mafalda Extensions","72108 Leuschke Greens"]
-# puts apartment_address
-# puts apartment_address.inspect
-#
-# apartment_id.each do |e|
-#     puts e
-# end
+apartment_address = ["9841 Tanner Key","630 McDermott Islands","2100 Mitchel Valleys","37899 Kutch Corners","29724 Ryan Ranch","80498 Mafalda Extensions","72108 Leuschke Greens"]
+puts apartment_address
+puts apartment_address.inspect
+
+apartment_id.each do |e|
+    puts e
+end
 
 # Print only tenants that are over the age of 44
-# tenant_name= ["Ms. Maryse Farrell","Jordan Cremin DDS","Haven Abbott","Roxane Walter","Dr. Everett Kuhn","Miss Flavio Wyman","Mathilde Beatty","Randall Crist Sr.","Connor Kunze","Dessie McLaughlin","Buck Jacobs PhD","Una Kulas","Elenora Dibber","Lavern Ruecker","Dolly Davis","Jaida Runte","Elmo Wilkinson Sr.","Mrs. Cloyd Swift","Albin Treutel","Thad Spinka","Emmanuelle Koss","Hope Reilly","Domenic Hauck","Assunta Wintheiser","Evalyn Durgan","Miss Jed Murazik","Miss Abbie Wolf","Gene Purdy","Ezequiel Powlowski","Mikel Bashirian Sr.","Mrs. Rosie Labadie","Lacey Gerhold","Dr. Ruby Mayer","Elody Bogisich MD","Raven McGlynn","Rowland Wiegand","Rigoberto Armstrong IV"]
-# puts tenant_name
-# puts tenant_name.inspect
-#
-# tenant_name.each do |e|
-#   puts e
-# end
+tenant_name= ["Ms. Maryse Farrell","Jordan Cremin DDS","Haven Abbott","Roxane Walter","Dr. Everett Kuhn","Miss Flavio Wyman","Mathilde Beatty","Randall Crist Sr.","Connor Kunze","Dessie McLaughlin","Buck Jacobs PhD","Una Kulas","Elenora Dibber","Lavern Ruecker","Dolly Davis","Jaida Runte","Elmo Wilkinson Sr.","Mrs. Cloyd Swift","Albin Treutel","Thad Spinka","Emmanuelle Koss","Hope Reilly","Domenic Hauck","Assunta Wintheiser","Evalyn Durgan","Miss Jed Murazik","Miss Abbie Wolf","Gene Purdy","Ezequiel Powlowski","Mikel Bashirian Sr.","Mrs. Rosie Labadie","Lacey Gerhold","Dr. Ruby Mayer","Elody Bogisich MD","Raven McGlynn","Rowland Wiegand","Rigoberto Armstrong IV"]
+puts tenant_name
+puts tenant_name.inspect
+
+tenant_name.each do |e|
+  puts e
+end
 
 # Print only tenants that have an apartment id of 1
-# tenant = [:id=>1, :name=>"Ms. Maryse Farrell", :age=>87, :apartment_id=>8]
-# puts Ms Maryse Farrell
-# end
+tenant = [:id=>1, :name=>"Ms. Maryse Farrell", :age=>87, :apartment_id=>8]
+puts Ms Maryse Farrell
+end
 
 
   # Print all the tenants in order from youngest to oldest
-# tenants.sort { |a, b| a[:age] <=> b[:age] }
-   # youngest to oldest
+tenants.sort { |a, b| a[:age] <=> b[:age] }
+   youngest to oldest
 
 
 # Print the names of all the tenants alphabetically
-# tenants.sort { |a, b| a|[:name] <=> b[:name] }
-   #alphabetically
+tenants.sort { |a, b| a|[:name] <=> b[:name] }
+   alphabetically
 
   # Print all the addresses for the apartments
   # Print all the names for tenants
