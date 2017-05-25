a77651532bfcbfec6c17b4284da50245bfb6c29a
Tim Chase <tchase4544@gmail.com>
2017-05-24 21:05:17 -0400

    MVP done

--- app.rb
+++ app.rb
@@ -10,44 +10,84 @@ tenants = data[:tenants]
   #    id,name,age,apartment_id for tenants
 
 # Use enumerables to -
-  # Print all the addresses for the apartments
-  apartments.each_with_index do |value, index|
-    puts " #{value[:address]}"
-  end
-  # Print all the names for tenants
-  tenants.each_with_index do |value, index|
-    puts " #{value[:name]}"
-  end
-  # Print only apartments that are less then 700 in rent
-  apartments.each_with_index do |value, index|
-    if value[:monthly_rent] < 700
-      puts " #{value}"
-    end
-  end
-  # Print only tenants that are over the age of 44
-  tenants.each_with_index do |value, index|
-    if value[:age] > 44
-      puts " #{value}"
-    end
-  end
-  # Print only tenants that have an apartment id of 1
-  tenants.each_with_index do |value, index|
-    if value[:apartment_id] == 1
-      puts " #{value}"
-    end
-  end
-  # Print all the tenants in order from youngest to oldest
-  puts tenants.sort_by { |x| x[:age] }
-  # Print the names of all the tenants alphabetically
-  puts tenants.sort_by {|x| x[:name]}
+  # # Print all the addresses for the apartments
+  # apartments.each_with_index do |value, index|
+  #   puts " #{value[:address]}"
+  # end
+  # # Print all the names for tenants
+  # tenants.each_with_index do |value, index|
+  #   puts " #{value[:name]}"
+  # end
+  # # Print only apartments that are less then 700 in rent
+  # apartments.each_with_index do |value, index|
+  #   if value[:monthly_rent] < 700
+  #     puts " #{value}"
+  #   end
+  # end
+  # # Print only tenants that are over the age of 44
+  # tenants.each_with_index do |value, index|
+  #   if value[:age] > 44
+  #     puts " #{value}"
+  #   end
+  # end
+  # # Print only tenants that have an apartment id of 1
+  # tenants.each_with_index do |value, index|
+  #   if value[:apartment_id] == 1
+  #     puts " #{value}"
+  #   end
+  # end
+  # # Print all the tenants in order from youngest to oldest
+  # puts tenants.sort_by { |x| x[:age] }
+  # # Print the names of all the tenants alphabetically
+  # puts tenants.sort_by {|x| x[:name]}
 
-  ## More challenging
+  # ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
   # When printing all apartments, under each apartment print all of its tenants
 
+class Landlord
+  def initialize(one,two)
+    @apartments = one
+    @tenants = two
+  end
+  def display
+    while 1
+      puts "1 to display apartments"
+      puts "2 to display tenants"
+      puts "3"
+      puts "10 Exit"
+      input = gets.chomp!
+        if ["1","2","3","10"].include? input
+          menu_selection input
+          break
+        else
+          puts "Invalid option."
+        end
+    end
+  end
+
+  def menu_selection number
+    case number
+    when "1"
+      @apartments.each do |x|
+        puts x
+      end
+      display
+    when "2"
+      @tenants.each do |x|
+        puts x
+      end
+      display
+    when ""
+    when ""
+    else
+      return
+    end
+  end
+end
+
+landlord_app = Landlord.new(apartments,tenants)
+landlord_app.display
 
 
 
-
-
-  
