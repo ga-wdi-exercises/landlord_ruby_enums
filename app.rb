150cecc0540baa8fcf07db2f113fdf5b318de624
MATT FILBERT <mcfilbert@email.wm.edu>
2017-07-19 13:33:17 -0400

    solid gold for part 2

--- app.rb
+++ app.rb
@@ -11,6 +11,10 @@ while answer.upcase != "EXIT"
   puts "Type 2 to view an overview of properties."
   puts "Type 3 to view a list of properties and the tenants who live in each."
   puts "Type 4 to search for where a tenant lives by their name."
+  puts "Type 5 to evict a tenant (search by their name)."
+  puts "Type 6 to add a tenant to an apartment (search tenant by their name and apt by id)."
+  puts "Type 7 to add a new property."
+  puts "Type 8 to add a new tenant."
   puts "Type 0 for a personal message from Bob Pizza."
   puts "Type EXIT to leave."
   answer = gets.chomp
@@ -35,10 +39,47 @@ while answer.upcase != "EXIT"
     apartment_id = tenant[:apartment_id]
     apartment = apartments.find { |apt| apt[:id] == apartment_id}
     puts "#{input_name} lives at #{apartment[:address]}."
+  elsif answer == "5"
+    puts "What is the name of the tenant you are kicking out?"
+    input_name = gets.chomp
+    tenant = tenants.find { |ten| ten[:name] == input_name}
+    apartment_id = tenant[:apartment_id]
+    apartment = apartments.find { |apt| apt[:id] == apartment_id}
+    puts "#{input_name} no longer lives at #{apartment[:address]} and has been evicted."
+    tenant[:apartment_id] = 0
+  elsif answer == "6"
+    puts "What is the name of the tenant you are adding to an apartment?"
+    input_name = gets.chomp
+    tenant = tenants.find { |ten| ten[:name] == input_name}
+    puts "What is the id of the apartment you are adding the tenant to?"
+    tenant[:apartment_id] = gets.chomp.to_i
+    apartment_id = tenant[:apartment_id]
+    apartment = apartments.find { |apt| apt[:id] == apartment_id}
+    puts "#{input_name} now lives at #{apartment[:address]}."
+  elsif answer == "7"
+    puts "What is the address of your new apartment?"
+    address = gets.chomp
+    puts "What is the monthly rent of your new apartment?"
+    monthly_rent = gets.chomp.to_i
+    puts "What is the square footage of your new apartment?"
+    square_feet = gets.chomp.to_i
+    apartments.push({:id=>apartments.last[:id] + 1, :address=>address, :monthly_rent=>monthly_rent, :square_feet=>square_feet})
+    puts "#{address} is now an apartment with a monthly rent of #{monthly_rent} and a square footage of #{square_feet}."
+  elsif answer == "8"
+    puts "What is the name of your new tenant?"
+    name = gets.chomp
+    puts "What is the age of your new tenant?"
+    age = gets.chomp.to_i
+    puts "What is the id of their apartment? (Set to 0 if none.)"
+    apartment_id = gets.chomp.to_i
+    apartment = apartments.find { |apt| apt[:id] == apartment_id}
+    tenants.push({:id=>tenants.last[:id] + 1, :name=>name, :age=>age, :apartment_id=>apartment_id})
+    puts "#{name} is now a tenant who is #{age} years old who lives at #{apartment[:address]}."
   elsif answer == "0"
     puts "Bob Pizza is hot and greasy from a long day in the oven."
     puts "Bob Pizza only accepts the juciest, most tender sausages for his toppings."
     puts "Bob Pizza cries during, but that doesn't make him any less delicious."
+    puts "- Bob Pizza"
   elsif answer.upcase == "EXIT"
     puts "Bob Pizza sends his regards..."
   else
