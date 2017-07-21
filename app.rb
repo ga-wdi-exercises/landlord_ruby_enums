18faea4eee45a40d6995641745adf7531df388b7
Rami Taha <ramisn3@gmail.com>
2017-07-21 10:05:41 -0400

    update

--- app.rb
+++ app.rb
@@ -13,50 +13,43 @@ tenants = data[:tenants]
   # hash tenant : id, name, age, apartment_id
 
 # Use enumerables to -
-  # Print all the addresses for the apartments
-  # Print all the names for tenants
-  # Print only apartments that are less then 700 in rent
-  # Print only tenants that are over the age of 44
-  # Print only tenants that have an apartment id of 1
+
+
   # Print all the tenants in order from youngest to oldest
   # Print the names of all the tenants alphabetically
 
   ## More challenging
   # When printing tenants also print out the address that the tenant resides in.
   # When printing all apartments, under each apartment print all of its tenants
-  equire_relative "data"
+  require_relative "data"
    require "pry"
     apartments = data[:apartments]
     tenants = data[:tenants]
-
-    puts "Address for each Apt"
-     apartments.each do |apt|
-     print apt[:address] + "\n"
-     end
-     puts "Names of Each Tenant"
-     tenants.each do |tenant|
-       print tenant[:name] + "\n"
-     end
-     puts "Apt IDs of less than $700"
-     for apt in apartments
-       if apt[:monthly_rent] < 700
-         print apt[:id].to_s + "\n"
-       end
-     end
-     puts "Tenants over 44 years"
-     for tenant in tenants
-      if tenant[:age] > 44
-         print tenant[:name] + "\n"
-       end
-     end
-     puts "Tenants in Apt 1"
-     for tenant in tenants
-       if tenant[:apartment_id] == 1
-         print tenant[:name] + "\n"
-         print tenant[]
-       end
-     end
-     puts "Tenants by Age (young to old)"
-     print tenants.sort_by{ |a,b| a[:age] }
-     puts "Tenants alphabetically"
-     print tenants.sort_by{ |a,b| a[:name] }
+    # Print all the addresses for the apartments
+    apartments.each{|apt| puts "#{apt[:address]}"}
+    # Print all the names for tenants
+    tenants.each{|tenant| puts "#{tenant[:name]}"}
+    # Print only apartments that are less then 700 in rent
+for apt in apartments
+if apt[:monthly_rent] < 700
+  puts "#{apt[:id]}"
+end
+end
+# Print only tenants that are over the age of 44
+for tenant in tenants
+  if tenant[:age] > 44
+    puts "#{tenant[:name]}"
+  end
+end
+# Print only tenants that have an apartment id of 1
+for tenant in tenants
+  if tenant[:apartment_id] == 1
+    puts "#{tenant[:name]}"
+  end
+end
+   t_a = tenants[:name].sort_by{|a,b| a[:age]}
+   puts "#{t_a}"
+    #  puts "Tenants by Age (young to old)"
+    #  print tenants.sort_by{ |a,b| a[:age] }
+    #  puts "Tenants alphabetically"
+    #  print tenants.sort_by{ |a,b| a[:name] }
